550d214297044c934a3958e22a3fcb1099f88cb0
혜민 <lhm0710@naver.com>
2020-08-05 16:38:36 +0900

    installed storybook

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+
 export default function App() {
   return (
     <View style={styles.container}>
