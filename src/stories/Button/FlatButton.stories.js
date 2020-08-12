b84f640ecb68b318fe2239f8b841937ac17b77b4
hyunoh-modori <59865307+hyunoh-modori@users.noreply.github.com>
2020-08-12 18:53:19 +0900

    remove files

--- src/stories/Button/FlatButton.stories.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "./node_modules/@storybook/reactstorybook/react";
-import FlatButton from "../../src/shared/components/FlatButton";
-
-storiesOf("FlatButton", module)
-  .add("default", () => <FlatButton title={task.title} handler={handler} />);
