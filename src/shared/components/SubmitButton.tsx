4d50606447c80c6ccdcc666d2be078f6d6e2472e
hyunoh-modori <59865307+hyunoh-modori@users.noreply.github.com>
2020-08-31 20:02:41 +0900

    button Theme

--- src/shared/components/SubmitButton.tsx
+++ src/shared/components/SubmitButton.tsx
@@ -12,14 +12,29 @@ interface SubmitButtonProps {
   title: string;
   // handler: (event: GestureResponderEvent) => void;
   handler: any;
-  ButtonTheme?: string;
+  ButtonTheme?: string; // WhiteTheme를 받으면 하얀색 버튼으로 변경됩니다
 }
 
-export const SubmitButton: FunctionComponent<SubmitButtonProps> = ({ title, handler, ButtonTheme }) => {
+export const SubmitButton: FunctionComponent<SubmitButtonProps> = ({
+  title,
+  handler,
+  ButtonTheme,
+}) => {
+  const handleThemeType = (BtnTheme) => {
+    switch (BtnTheme) {
+      case "WhiteTheme":
+        return "border: 1px solid #3679B5; background-color: #FFFFFF;";
+      case "GrayTheme":
+        return "border: 1px solid #FFFFFF; background-color: #AAAAAA;";
+      default:
+        return "border: 1px solid #FFFFFF; background-color: #2c6190;";
+    }
+  };
   const WhiteBtnWrapper = styled.View`
-    flex: ${props => props.BtnTheme == "WhiteTheme" ? '4' : '1'};
+    flex: ${(props) => (props.BtnTheme == "WhiteTheme" ? "4" : "1")};
     justify-content: flex-end;
-    margin-bottom: ${props => props.BtnTheme == "WhiteTheme" ? '25px' : '36px'};
+    margin-bottom: ${(props) =>
+      props.BtnTheme == "WhiteTheme" ? "25px" : "36px"};
   `;
   const WhiteBtn = styled.TouchableOpacity`
     width: 90%;
@@ -30,22 +45,21 @@ export const SubmitButton: FunctionComponent<SubmitButtonProps> = ({ title, hand
     position: absolute;
     bottom: 0;
 
-    border: 1px solid ${props => props.BtnTheme == "WhiteTheme" ? '#3679B5' : '#FFFFFF'};
-    background-color: ${props => props.BtnTheme == "WhiteTheme" ? '#FFFFFF' : '#2c6190'};
+    ${({ BtnTheme }) => handleThemeType(BtnTheme)};
   `;
   const WhiteBtnText = styled.Text`
     font-size: 14px;
     text-align: center;
     line-height: 40px;
 
-    color: ${props => props.BtnTheme == "WhiteTheme" ? '#000000' : '#FFFFFF'};
+    color: ${(props) =>
+      props.BtnTheme == "WhiteTheme" ? "#000000" : "#FFFFFF"};
   `;
   return (
     <WhiteBtnWrapper BtnTheme={ButtonTheme}>
-      <WhiteBtn onPress={handler} BtnTheme={ButtonTheme} >
-        <WhiteBtnText BtnTheme={ButtonTheme} >{title}</WhiteBtnText>
+      <WhiteBtn onPress={handler} BtnTheme={ButtonTheme}>
+        <WhiteBtnText BtnTheme={ButtonTheme}>{title}</WhiteBtnText>
       </WhiteBtn>
     </WhiteBtnWrapper>
   );
 };
-
