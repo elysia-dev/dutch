6d20b9c62a692f0c0545c66661d43cb0777de4d8
hyunoh-modori <59865307+hyunoh-modori@users.noreply.github.com>
2020-10-08 17:48:13 +0900

    rebuild FAQ

--- src/modules/more/Faq.tsx
+++ src/modules/more/Faq.tsx
@@ -1,104 +1,41 @@
 import React, { FunctionComponent, useContext, useState } from 'react';
-import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
 import i18n from '../../i18n/i18n';
-import { BackButton } from '../../shared/components/BackButton';
-import { PText } from '../../shared/components/PText';
-import { TitleText } from '../../shared/components/TitleText';
 import WrapperLayout from '../../shared/components/WrapperLayout';
 import { FaqItem } from './components/FaqItem';
-import { H1Text } from '../../shared/components/H1Text';
-import Answer06Png from './images/answer06Img.png';
-
-const Answer06Img = styled.Image``;
 
 const Faq: FunctionComponent = () => {
   const navigation = useNavigation();
   const [state, setState] = useState({
-    selectNumber: '',
+    selectNumber: 0,
   });
-  const setQuestionNumber = (text: string) => {
+  const setQuestionNumber = (arrayNum: number) => {
     // eslint-disable-next-line no-unused-expressions
-    state.selectNumber !== text
-      ? setState({ selectNumber: text })
-      : setState({ selectNumber: '' });
+    state.selectNumber !== arrayNum
+      ? setState({ selectNumber: arrayNum })
+      : setState({ selectNumber: 0 });
   };
+  const ItemListing = Array(13)
+    .fill(0)
+    .map((_x, index) => {
+      return (
+        <FaqItem
+          faqId={index + 1}
+          handler={() => setQuestionNumber(index)}
+          question={i18n.t('FAQ.question.' + index)}
+          answer={i18n.t('FAQ.answer.' + index)}
+          isSelected={state.selectNumber === index}
+        />
+      );
+    });
   return (
     <WrapperLayout
       isScrolling={true}
-      isBackbutton={true}
-      title={
-        <>
-          <BackButton
-            handler={() => {
-              navigation.goBack();
-            }}
-          />
-          <TitleText label={i18n.t('more_label.faq')} />
-        </>
-      }
-      body={
-        <>
-          <FaqItem
-            faqId={'01'}
-            handler={() => setQuestionNumber('01')}
-            question={i18n.t('FAQ.question.0')}
-            answer={<PText label={i18n.t('FAQ.answer.01')} />}
-            isSelected={state.selectNumber === '01'}
-          />
-          <FaqItem
-            faqId={'02'}
-            handler={() => setQuestionNumber('02')}
-            question={i18n.t('FAQ.question.1')}
-            answer={<PText label={i18n.t('FAQ.answer.02')} />}
-            isSelected={state.selectNumber === '02'}
-          />
-          <FaqItem
-            faqId={'03'}
-            handler={() => setQuestionNumber('03')}
-            question={i18n.t('FAQ.question.2')}
-            answer={<PText label={i18n.t('FAQ.answer.03')} />}
-            isSelected={state.selectNumber === '03'}
-          />
-          <FaqItem
-            faqId={'04'}
-            handler={() => setQuestionNumber('04')}
-            question={i18n.t('FAQ.question.3')}
-            answer={<PText label={i18n.t('FAQ.answer.04')} />}
-            isSelected={state.selectNumber === '04'}
-          />
-          <FaqItem
-            faqId={'05'}
-            handler={() => setQuestionNumber('05')}
-            question={i18n.t('FAQ.question.4')}
-            answer={<PText label={i18n.t('FAQ.answer.05')} />}
-            isSelected={state.selectNumber === '05'}
-          />
-          <FaqItem
-            faqId={'06'}
-            handler={() => setQuestionNumber('06')}
-            question={i18n.t('FAQ.question.5')}
-            answer={
-              <>
-                <PText label={i18n.t('FAQ.answer.06')} />
-                <Answer06Img
-                  source={Answer06Png}
-                  style={{ resizeMode: 'center', width: '100%' }}
-                />
-                <PText label={i18n.t('FAQ.answer.06-1')} />
-              </>
-            }
-            isSelected={state.selectNumber === '06'}
-          />
-          <FaqItem
-            faqId={'07'}
-            handler={() => setQuestionNumber('07')}
-            question={i18n.t('FAQ.question.6')}
-            answer={<PText label={i18n.t('FAQ.answer.07')} />}
-            isSelected={state.selectNumber === '07'}
-          />
-        </>
-      }
+      backButtonHandler={() => {
+        navigation.goBack();
+      }}
+      title={i18n.t('more_label.faq')}
+      body={ItemListing}
     />
   );
 };
--- src/modules/more/components/FaqItem.tsx
+++ src/modules/more/components/FaqItem.tsx
@@ -1,18 +1,17 @@
-import React, { createRef, FunctionComponent, useState } from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import React, { FunctionComponent, useState } from 'react';
+import { View } from 'react-native';
 
 import styled from 'styled-components/native';
 import { H1Text } from '../../../shared/components/H1Text';
 import { PText } from '../../../shared/components/PText';
-import { Story } from '../../../types/product';
 import UpperArrowPng from '../images/upperArrow.png';
 import lowerArrowPng from '../images/lowerArrow.png';
 
 interface Props {
-  faqId: string;
+  faqId: number;
   handler: any;
   question: string;
-  answer: any;
+  answer: string;
   isSelected: boolean;
 }
 const QuestionWrapper = styled.TouchableOpacity`
@@ -31,17 +30,33 @@ const ArrowImg = styled.Image`
 `;
 
 export const FaqItem: FunctionComponent<Props> = (props: Props) => {
+  const [state, setIdValue] = useState({
+    selectID: '',
+  });
+  const ZeroFill = (num: string) => {
+    // eslint-disable-next-line no-unused-expressions
+    num.length === 1 ? (state.selectID = '0' + num) : (state.selectID = num);
+    return state.selectID;
+  };
   return (
     <View>
       <QuestionWrapper onPress={props.handler}>
         <H1Text
-          label={props.faqId}
-          style={{ flex: 1, fontSize: 15, color: '#3679B5' }}
+          label={ZeroFill(props.faqId.toString())}
+          style={{
+            flex: 1,
+            fontSize: 15,
+            color: '#3679B5',
+          }}
         />
         <PText label={props.question} style={{ flex: 10, fontSize: 15 }} />
         <ArrowImg source={props.isSelected ? UpperArrowPng : lowerArrowPng} />
       </QuestionWrapper>
-      {props.isSelected && <AnswerWrapper>{props.answer}</AnswerWrapper>}
+      {props.isSelected && (
+        <AnswerWrapper>
+          <PText label={props.answer} style={{ fontSize: 13 }} />
+        </AnswerWrapper>
+      )}
     </View>
   );
 };
