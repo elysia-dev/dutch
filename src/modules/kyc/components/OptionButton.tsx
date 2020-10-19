99ba01dc2606662f48a09552714b7286449d18a5
hyemmie <lhm0710@naver.com>
2020-10-19 19:36:06 +0900

    fix kyc optionbutton align

--- src/modules/kyc/components/OptionButton.tsx
+++ src/modules/kyc/components/OptionButton.tsx
@@ -8,6 +8,10 @@ import {
 } from "react-native";
 import styled from "styled-components/native";
 
+interface ButtonProps {
+  checking: string;
+}
+
 const OptionBtn = styled.TouchableOpacity`
   color: #000;
   width: 90%;
@@ -16,7 +20,7 @@ const OptionBtn = styled.TouchableOpacity`
   background-color: #fff;
   border-radius: 5px;
   border: solid 1px
-    ${(props) => (props.checking == "selected" ? "#3679B5" : "#d0d8df")};
+    ${(props: ButtonProps) => (props.checking === "selected" ? "#3679B5" : "#d0d8df")};
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -28,13 +32,14 @@ const OptionBtnText = styled.Text`
   text-align: center;
   line-height: 40px;
   z-index: 5;
+  align-self: center;
 `;
 const OptionBtnChecked = styled.View`
   flex-direction: row;
   margin-left: auto;
 `;
 
-interface props {
+interface Props {
   title: string;
   handler: any;
   child: any;
@@ -42,10 +47,10 @@ interface props {
   selected?: string;
 }
 
-export const OptionButton: FunctionComponent<props> = (props) => {
+export const OptionButton: FunctionComponent<Props> = (props: Props) => {
   return (
     <View>
-      <OptionBtn onPress={props.handler} checking={props.selected}>
+      <OptionBtn onPress={props.handler} checking={props.selected ? props.selected : ""}>
         {props.child}
         <OptionBtnText>{props.title}</OptionBtnText>
         <OptionBtnChecked>{props.checked}</OptionBtnChecked>
