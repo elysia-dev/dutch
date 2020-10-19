5d8d674061494dc2c330269397e6200c75b2fb4a
hyunoh-modori <59865307+hyunoh-modori@users.noreply.github.com>
2020-10-19 19:36:13 +0900

    fix error & add circle event

--- src/modules/dashboard/InvestmentGuide.tsx
+++ src/modules/dashboard/InvestmentGuide.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useState } from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, SegmentedControlIOS } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import ViewPager from '@react-native-community/viewpager';
 import styled from 'styled-components/native';
 import investmentGuide1png from './images/investmentGuide1.png';
@@ -28,6 +28,8 @@ const Circle = styled.View`
 
 const InvestmentGuide: FunctionComponent<{}> = () => {
   const navigation = useNavigation();
+  const [state, setState] = useState(0);
+  const viewPager = React.createRef<ViewPager>();
 
   return (
     <View style={{ flex: 1, backgroundColor: "#FFF", paddingTop: 55 }}>
@@ -44,29 +46,33 @@ const InvestmentGuide: FunctionComponent<{}> = () => {
             source={QuitIcon}
           />
         </TouchableOpacity>
-      <ViewPager style={styles.viewPager} initialPage={0}>
-        <View style={styles.page} key="1">
+      <ViewPager style={styles.viewPager} initialPage={0} ref={viewPager}
+        onPageSelected={e => {
+          setState(e.nativeEvent.position);
+        }}
+        >
+        <View style={styles.page}>
           <GuideImage source={investmentGuide1png} />
           <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
             <H1Text label={'EL토큰 입금하기'} style={{ fontSize: 25, textAlign: "center" }} />
             <PText label={'상장거래소에서 EL을 구입 후, \n본인의 인증지갑주소로 EL을 입급해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
           </View>
         </View>
-        <View style={styles.page} key="2">
+        <View style={styles.page}>
         <GuideImage source={investmentGuide2png} />
           <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
             <H1Text label={'상품&수량 선택하기'} style={{ fontSize: 25, textAlign: "center" }} />
             <PText label={'마음에 드는 상품을 선택하신후,\n투자할 부동산 토큰 수량을 정해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
           </View>
         </View>
-        <View style={styles.page} key="3">
+        <View style={styles.page}>
         <GuideImage source={investmentGuide3png} />
           <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
             <H1Text label={'트랜잭션 만들기'} style={{ fontSize: 25, textAlign: "center" }} />
             <PText label={'상장거래소에서 EL을 구입 후, \n본인의 인증지갑주소로 EL을 입급해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
           </View>
         </View>
-        <View style={styles.page} key="4">
+        <View style={styles.page} >
         <GuideImage source={investmentGuide4png} />
           <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
             <H1Text label={'지분소유 완료!'} style={{ fontSize: 25, textAlign: "center" }} />
@@ -75,10 +81,18 @@ const InvestmentGuide: FunctionComponent<{}> = () => {
         </View>
       </ViewPager>
       <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'row', bottom: "20%" }}>
-        <Circle />
-        <Circle />
-        <Circle />
-        <Circle />
+        <TouchableOpacity onPress={() => { viewPager.current?.setPage(0); }}>
+          <Circle style={[state === 0 ? styles.enableCircle : styles.disableCircle]}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { viewPager.current?.setPage(1); }}>
+          <Circle style={[state === 1 ? styles.enableCircle : styles.disableCircle]}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { viewPager.current?.setPage(2); }}>
+          <Circle style={[state === 2 ? styles.enableCircle : styles.disableCircle]}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { viewPager.current?.setPage(3); }}>
+          <Circle style={[state === 3 ? styles.enableCircle : styles.disableCircle]}/>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -88,6 +102,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   page: {
+  },
+  enableCircle: {
+    backgroundColor: "#3679B5",
+  },
+  disableCircle: {
+    backgroundColor: "#BDD3E6",
   },
 });
 
