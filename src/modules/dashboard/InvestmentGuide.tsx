1afd96d59293ae388313e8e3be7e0a8e8f2605dd
hyunoh-modori <59865307+hyunoh-modori@users.noreply.github.com>
2020-10-19 20:05:11 +0900

    add i18n & code optimization

--- src/modules/dashboard/InvestmentGuide.tsx
+++ src/modules/dashboard/InvestmentGuide.tsx
@@ -3,13 +3,14 @@ import { StyleSheet, View, Image, TouchableOpacity, SegmentedControlIOS } from '
 import { useNavigation } from '@react-navigation/native';
 import ViewPager from '@react-native-community/viewpager';
 import styled from 'styled-components/native';
-import investmentGuide1png from './images/investmentGuide1.png';
-import investmentGuide2png from './images/investmentGuide2.png';
-import investmentGuide3png from './images/investmentGuide3.png';
-import investmentGuide4png from './images/investmentGuide4.png';
+import investmentGuide1 from './images/investmentGuide1.png';
+import investmentGuide2 from './images/investmentGuide2.png';
+import investmentGuide3 from './images/investmentGuide3.png';
+import investmentGuide4 from './images/investmentGuide4.png';
 import { H1Text } from '../../shared/components/H1Text';
 import { PText } from '../../shared/components/PText';
 import QuitIcon from '../products/images/quitbutton.png';
+import i18n from '../../i18n/i18n';
 
 const GuideImage = styled.Image`
   width: 90%;
@@ -30,7 +31,36 @@ const InvestmentGuide: FunctionComponent<{}> = () => {
   const navigation = useNavigation();
   const [state, setState] = useState(0);
   const viewPager = React.createRef<ViewPager>();
-
+  const ReturnImage = (imgNumber: number) => {
+    switch (imgNumber) {
+      case 0: return investmentGuide1;
+      case 1: return investmentGuide2;
+      case 2: return investmentGuide3;
+      default: return investmentGuide4;
+    }
+  };
+  const ItemListing = Array(4)
+    .fill(0)
+    .map((_x, index) => {
+      return (
+        <View style={styles.page}>
+          <GuideImage source={ReturnImage(index)} />
+          <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
+            <H1Text label={i18n.t('dashboard.investment_guide_header.' + index)} style={{ fontSize: 25, textAlign: "center" }} />
+            <PText label={i18n.t('dashboard.investment_guide_text.' + index)} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
+          </View>
+        </View>
+      );
+    });
+  const ButtonListing = Array(4)
+    .fill(0)
+    .map((_x, index) => {
+      return (
+        <TouchableOpacity onPress={() => { viewPager.current?.setPage(index); }}>
+          <Circle style={[state === index ? styles.enableCircle : styles.disableCircle]}/>
+        </TouchableOpacity>
+      );
+    });
   return (
     <View style={{ flex: 1, backgroundColor: "#FFF", paddingTop: 55 }}>
       <TouchableOpacity
@@ -51,48 +81,10 @@ const InvestmentGuide: FunctionComponent<{}> = () => {
           setState(e.nativeEvent.position);
         }}
         >
-        <View style={styles.page}>
-          <GuideImage source={investmentGuide1png} />
-          <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
-            <H1Text label={'EL토큰 입금하기'} style={{ fontSize: 25, textAlign: "center" }} />
-            <PText label={'상장거래소에서 EL을 구입 후, \n본인의 인증지갑주소로 EL을 입급해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
-          </View>
-        </View>
-        <View style={styles.page}>
-        <GuideImage source={investmentGuide2png} />
-          <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
-            <H1Text label={'상품&수량 선택하기'} style={{ fontSize: 25, textAlign: "center" }} />
-            <PText label={'마음에 드는 상품을 선택하신후,\n투자할 부동산 토큰 수량을 정해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
-          </View>
-        </View>
-        <View style={styles.page}>
-        <GuideImage source={investmentGuide3png} />
-          <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
-            <H1Text label={'트랜잭션 만들기'} style={{ fontSize: 25, textAlign: "center" }} />
-            <PText label={'상장거래소에서 EL을 구입 후, \n본인의 인증지갑주소로 EL을 입급해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
-          </View>
-        </View>
-        <View style={styles.page} >
-        <GuideImage source={investmentGuide4png} />
-          <View style={{ marginTop: 'auto', marginBottom: "35%" }}>
-            <H1Text label={'지분소유 완료!'} style={{ fontSize: 25, textAlign: "center" }} />
-            <PText label={'상장거래소에서 EL을 구입 후, \n본인의 인증지갑주소로 EL을 입급해주세요!'} style={{ fontSize: 15, marginTop: 20, textAlign: "center" }} />
-          </View>
-        </View>
+        {ItemListing}
       </ViewPager>
       <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'row', bottom: "20%" }}>
-        <TouchableOpacity onPress={() => { viewPager.current?.setPage(0); }}>
-          <Circle style={[state === 0 ? styles.enableCircle : styles.disableCircle]}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => { viewPager.current?.setPage(1); }}>
-          <Circle style={[state === 1 ? styles.enableCircle : styles.disableCircle]}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => { viewPager.current?.setPage(2); }}>
-          <Circle style={[state === 2 ? styles.enableCircle : styles.disableCircle]}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => { viewPager.current?.setPage(3); }}>
-          <Circle style={[state === 3 ? styles.enableCircle : styles.disableCircle]}/>
-        </TouchableOpacity>
+        {ButtonListing}
       </View>
     </View>
   );
