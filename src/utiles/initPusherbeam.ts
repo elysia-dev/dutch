90e060258fee4b92cddde98513230987760eff18
donguk.seo <donguk.seo@modori.io>
2020-10-23 02:49:15 +0900

    Install device & deactive pusher beam

--- src/utiles/initPusherbeam.ts
+++ src/utiles/initPusherbeam.ts
@@ -1,7 +1,11 @@
 import RNPusherPushNotifications from "react-native-pusher-push-notifications";
+import * as Device from 'expo-device';
 import getEnvironment from "./getEnvironment";
 
+const canUsePussherBeam = Device.isDevice && getEnvironment().envName !== 'DEVELOPMENT';
+
 const initPusherBeam = (email: string): void => {
+  /*
   RNPusherPushNotifications.setInstanceId(getEnvironment().pusherBeamInstanceId);
 
   RNPusherPushNotifications.subscribe(
@@ -11,13 +15,17 @@ const initPusherBeam = (email: string): void => {
     },
     () => { },
   );
+  */
 };
 
 export const clearPusehrBeam = () => {
+  /*
   RNPusherPushNotifications.clearAllState();
+  */
 };
 
 export const unsubscribePusherBeam = (email: string): void => {
+  /*
   RNPusherPushNotifications.unsubscribe(
     email,
     (_statusCode: object, _response: object) => {
@@ -25,6 +33,7 @@ export const unsubscribePusherBeam = (email: string): void => {
     },
     () => { },
   );
+  */
 };
 
 export default initPusherBeam;
