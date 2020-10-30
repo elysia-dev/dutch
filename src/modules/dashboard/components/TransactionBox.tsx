2b70d027578985a23b4846a489255f4cf73dc074
hyemmie <lhm0710@naver.com>
2020-10-30 11:58:08 +0900

    edit to show transaction button

--- src/modules/dashboard/components/TransactionBox.tsx
+++ src/modules/dashboard/components/TransactionBox.tsx
@@ -4,7 +4,12 @@ import styled from 'styled-components/native';
 import * as Linking from 'expo-linking';
 import i18n from '../../../i18n/i18n';
 import { Transaction } from '../../../types/Transaction';
-import { P1Text, P3Text, P4Text, H3Text } from '../../../shared/components/Texts';
+import {
+  P1Text,
+  P3Text,
+  P4Text,
+  H3Text,
+} from '../../../shared/components/Texts';
 
 interface Props {
   transaction: Transaction;
@@ -57,43 +62,46 @@ export const TransactionBox: FunctionComponent<Props> = (props: Props) => {
             {props.transaction.hash}
           </Text>
         )}
-        <TouchableOpacity
-          onPress={() => setState({ ...state, show: !state.show })}
-          style={{
-            backgroundColor: '#A7A7A7',
-            borderRadius: 2,
-            width: 60,
-            height: 20,
-            justifyContent: 'center',
-            alignContent: 'center',
-          }}>
-          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-            <P4Text style={{ color: '#fff', textAlign: 'center' }}
-              label={state.show
-                ? i18n.t('dashboard_label.fold')
-                : i18n.t('dashboard_label.transaction')} />
-            <Image
-              style={{
-                width: 5,
-                height: 3,
-                marginTop: 'auto',
-                marginBottom: 'auto',
-                marginLeft: 3,
-              }}
-              source={
-                state.show
-                  ? require('../images/whiteupbutton.png')
-                  : require('../images/whitedownbutton.png')
-              }></Image>
-          </View>
-        </TouchableOpacity>
+        {props.transaction.hash?.length > 0 && (
+          <TouchableOpacity
+            onPress={() => setState({ ...state, show: !state.show })}
+            style={{
+              backgroundColor: '#A7A7A7',
+              borderRadius: 2,
+              width: 60,
+              height: 20,
+              justifyContent: 'center',
+              alignContent: 'center',
+            }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+              <P4Text
+                style={{ color: '#fff', textAlign: 'center' }}
+                label={
+                  state.show
+                    ? i18n.t('dashboard_label.fold')
+                    : i18n.t('dashboard_label.transaction')
+                }
+              />
+              <Image
+                style={{
+                  width: 5,
+                  height: 3,
+                  marginTop: 'auto',
+                  marginBottom: 'auto',
+                  marginLeft: 3,
+                }}
+                source={
+                  state.show
+                    ? require('../images/whiteupbutton.png')
+                    : require('../images/whitedownbutton.png')
+                }></Image>
+            </View>
+          </TouchableOpacity>
+        )}
       </View>
 
       <View
-        style={{ flex: 1,
-          justifyContent: 'center',
-          alignContent: 'center',
-        }}>
+        style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
         <H3Text
           style={{
             fontSize: 17,
@@ -105,12 +113,14 @@ export const TransactionBox: FunctionComponent<Props> = (props: Props) => {
                 ? '#1C1C1C'
                 : '#3679B5',
           }}
-          label=
-          {props.transaction.transactionType === 'refund' ||
-          props.transaction.transactionType === 'close' ||
-          props.transaction.transactionType === 'profit'
-            ? `- $${parseFloat(props.transaction.value).toFixed(2)}`
-            : `$${parseFloat(props.transaction.value).toFixed(2)}`} />
+          label={
+            props.transaction.transactionType === 'refund' ||
+            props.transaction.transactionType === 'close' ||
+            props.transaction.transactionType === 'profit'
+              ? `- $${parseFloat(props.transaction.value).toFixed(2)}`
+              : `$${parseFloat(props.transaction.value).toFixed(2)}`
+          }
+        />
       </View>
     </View>
   );
