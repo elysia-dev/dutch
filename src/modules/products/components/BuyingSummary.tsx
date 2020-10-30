d4d7f570ecce7782bb5c30477037c62da49484cd
hyemmie <lhm0710@naver.com>
2020-10-30 16:34:47 +0900

    add allowfontscaling option

--- src/modules/products/components/BuyingSummary.tsx
+++ src/modules/products/components/BuyingSummary.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { View, Image } from 'react-native';
 import styled from 'styled-components/native';
 import RootContext from '../../../contexts/RootContext';
@@ -7,71 +12,75 @@ import { H1Text } from '../../../shared/components/Texts';
 import Product from '../../../types/Product';
 
 const GrayBox = styled.View`
-display: flex;
-flex-direction:column;
-align-content: space-between;
-width: 100%;
-height: 175px;
-padding: 10px;
-border-radius: 10px;
-background-color: #F6F6F8;
-border-width:1px;
-border-color: #E5E5E5;
-margin-left: auto;
-margin-right:auto;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  width: 100%;
+  height: 175px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #f6f6f8;
+  border-width: 1px;
+  border-color: #e5e5e5;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const WhiteBox = styled.View`
-width: 100%;
-height: 65px;
-border-radius:10px;
-background-color: #fff;
-padding:10px;
-display: flex;
-flex-direction: column;
-align-content: space-between;
-
+  width: 100%;
+  height: 65px;
+  border-radius: 10px;
+  background-color: #fff;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
 `;
 const GrayText = styled.Text`
-font-size: 13px;
-text-align: center;
-align-items: center;
-color: #626368`;
+  font-size: 13px;
+  text-align: center;
+  align-items: center;
+  color: #626368;
+`;
 
 const BlackText = styled.Text`
-font-size: 13px;
-text-align: center;
-align-items: center;
-color: #1C1C1C`;
+  font-size: 13px;
+  text-align: center;
+  align-items: center;
+  color: #1c1c1c;
+`;
 
 const TextWrapper = styled.View`
-flex:1;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 type Props = {
   product: Product;
   tokenCount: number;
-}
+};
 
 const BuyingSummary: FunctionComponent<Props> = (props: Props) => {
   const [elPrice, setELPrice] = useState(0.003);
   const { Server } = useContext(RootContext);
 
-  const expectedUsdValue = (props.tokenCount || 0)
-    * parseFloat(`${props.product.usdPricePerToken}`);
+  const expectedUsdValue =
+    (props.tokenCount || 0) * parseFloat(`${props.product.usdPricePerToken}`);
   const expectedElValue = expectedUsdValue / elPrice;
 
   const expectedProfit = (
-    parseFloat(props.product.expectedAnnualReturn) * 0.01 * expectedUsdValue
+    parseFloat(props.product.expectedAnnualReturn) *
+    0.01 *
+    expectedUsdValue
   ).toFixed(2);
 
   useEffect(() => {
-    Server.getELPrice().then(res => setELPrice(res.data.elysia.usd)).catch(e =>
-      alert(i18n.t('account_errors.server')));
+    Server.getELPrice()
+      .then((res) => setELPrice(res.data.elysia.usd))
+      .catch((e) => alert(i18n.t('account_errors.server')));
   }, []);
 
   return (
@@ -79,50 +88,137 @@ const BuyingSummary: FunctionComponent<Props> = (props: Props) => {
       <GrayBox>
         <WhiteBox>
           <TextWrapper>
-            <GrayText style={{ flex: 1, textAlign: 'left' }} > FROM </GrayText>
-            <GrayText style={{ flex: 1 }}> TO </GrayText>
-            <GrayText style={{ flex: 3, textAlign: 'right' }}> VALUE </GrayText>
+            <GrayText
+              allowFontScaling={false}
+              style={{ flex: 1, textAlign: 'left' }}>
+              FROM
+            </GrayText>
+            <GrayText allowFontScaling={false} style={{ flex: 1 }}>
+              TO
+            </GrayText>
+            <GrayText
+              allowFontScaling={false}
+              style={{ flex: 3, textAlign: 'right' }}>
+              VALUE
+            </GrayText>
           </TextWrapper>
           <View style={{ flex: 1 }}>
-            <View style={{ position: 'relative', top: '50%', width: "100%", height: 1, backgroundColor: "#E5E5E5" }}></View>
+            <View
+              style={{
+                position: 'relative',
+                top: '50%',
+                width: '100%',
+                height: 1,
+                backgroundColor: '#E5E5E5',
+              }}></View>
           </View>
-          <TextWrapper >
-            <BlackText style={{ flex: 1, textAlign: 'left' }}> YOU </BlackText>
-            <BlackText style={{ flex: 1 }}> ELYSIA </BlackText>
-            <View style={{ flexDirection: "row", flex: 3, justifyContent: "flex-end" }}>
-              <BlackText style={{ textAlign: "right" }}>EL</BlackText>
-              <BlackText style={{ fontWeight: 'bold', textAlign: "right" }}> {expectedElValue.toFixed(2)} </BlackText>
+          <TextWrapper>
+            <BlackText
+              allowFontScaling={false}
+              style={{ flex: 1, textAlign: 'left' }}>
+              YOU
+            </BlackText>
+            <BlackText allowFontScaling={false} style={{ flex: 1 }}>
+              ELYSIA
+            </BlackText>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 3,
+                justifyContent: 'flex-end',
+              }}>
+              <BlackText
+                allowFontScaling={false}
+                style={{ textAlign: 'right' }}>
+                EL
+              </BlackText>
+              <BlackText
+                allowFontScaling={false}
+                style={{ fontWeight: 'bold', textAlign: 'right' }}>
+                {expectedElValue.toFixed(2)}
+              </BlackText>
             </View>
           </TextWrapper>
         </WhiteBox>
-        <View style={{ width: 30, height: 21, marginLeft: 'auto', marginRight: 'auto', padding: 7 }}>
-          <Image style={{ marginLeft: 'auto', marginRight: 'auto' }} source={require('../images/downbutton.png')} />
+        <View
+          style={{
+            width: 30,
+            height: 21,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            padding: 7,
+          }}>
+          <Image
+            style={{ marginLeft: 'auto', marginRight: 'auto' }}
+            source={require('../images/downbutton.png')}
+          />
         </View>
         <WhiteBox>
           <TextWrapper>
-            <GrayText style={{ flex: 1, textAlign: 'left' }} > FROM </GrayText>
-            <GrayText style={{ flex: 1 }}> TO </GrayText>
-            <GrayText style={{ flex: 3, textAlign: 'right' }}> VALUE </GrayText>
+            <GrayText
+              allowFontScaling={false}
+              style={{ flex: 1, textAlign: 'left' }}>
+              FROM
+            </GrayText>
+            <GrayText allowFontScaling={false} style={{ flex: 1 }}>
+              TO
+            </GrayText>
+            <GrayText
+              allowFontScaling={false}
+              style={{ flex: 3, textAlign: 'right' }}>
+              VALUE
+            </GrayText>
           </TextWrapper>
           <View style={{ flex: 1 }}>
-            <View style={{ position: 'relative', top: '50%', width: "100%", height: 1, backgroundColor: "#E5E5E5" }}></View>
+            <View
+              style={{
+                position: 'relative',
+                top: '50%',
+                width: '100%',
+                height: 1,
+                backgroundColor: '#E5E5E5',
+              }}></View>
           </View>
           <TextWrapper>
-            <BlackText style={{ flex: 1, textAlign: 'left' }}> ELYSIA </BlackText>
-            <BlackText style={{ flex: 1 }}> YOU </BlackText>
-            <View style={{ flexDirection: "row", flex: 3, justifyContent: "flex-end" }}>
-              <BlackText style={{ textAlign: "right" }}>{props.product.tokenName}</BlackText>
-              <BlackText style={{ fontWeight: 'bold', textAlign: "right" }}> {props.tokenCount}</BlackText>
+            <BlackText
+              allowFontScaling={false}
+              style={{ flex: 1, textAlign: 'left' }}>
+              ELYSIA
+            </BlackText>
+            <BlackText allowFontScaling={false} style={{ flex: 1 }}>
+              YOU
+            </BlackText>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 3,
+                justifyContent: 'flex-end',
+              }}>
+              <BlackText
+                allowFontScaling={false}
+                style={{ textAlign: 'right' }}>
+                {props.product.tokenName}
+              </BlackText>
+              <BlackText
+                allowFontScaling={false}
+                style={{ fontWeight: 'bold', textAlign: 'right' }}>
+                {props.tokenCount}
+              </BlackText>
             </View>
           </TextWrapper>
         </WhiteBox>
       </GrayBox>
-      <View style={{ paddingHorizontal: 10, flexDirection: "row", justifyContent: "space-between", marginTop: 15 }}>
-        <H1Text style={{ fontSize: 15 }} label={i18n.t('product_label.expected_return')}></H1Text>
+      <View
+        style={{
+          paddingHorizontal: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: 15,
+        }}>
         <H1Text
           style={{ fontSize: 15 }}
-          label={`$ ${expectedProfit}`}
-        />
+          label={i18n.t('product_label.expected_return')}></H1Text>
+        <H1Text style={{ fontSize: 15 }} label={`$ ${expectedProfit}`} />
       </View>
     </View>
   );
--- src/modules/products/components/InterestSummary.tsx
+++ src/modules/products/components/InterestSummary.tsx
@@ -4,55 +4,56 @@ import styled from 'styled-components/native';
 import Product from '../../../types/Product';
 
 const GrayBox = styled.View`
-display: flex;
-flex-direction:column;
-align-content: space-between;
-width: 100%;
-height: 90px;
-padding: 10px;
-border-radius: 10px;
-background-color: #F6F6F8;
-border-width:1px;
-border-color: #E5E5E5;
-margin-left: auto;
-margin-right:auto;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  width: 100%;
+  height: 90px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #f6f6f8;
+  border-width: 1px;
+  border-color: #e5e5e5;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const WhiteBox = styled.View`
-width: 100%;
-height: 70px;
-border-radius:10px;
-background-color: #fff;
-padding:10px;
-display: flex;
-flex-direction: column;
-align-content: space-between;
-
+  width: 100%;
+  height: 70px;
+  border-radius: 10px;
+  background-color: #fff;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
 `;
 const GrayText = styled.Text`
-font-size: 13px;
-text-align: center;
-align-items: center;
-color: #626368`;
+  font-size: 13px;
+  text-align: center;
+  align-items: center;
+  color: #626368;
+`;
 
 const BlackText = styled.Text`
-font-size: 13px;
-text-align: center;
-align-items: center;
-color: #1C1C1C`;
+  font-size: 13px;
+  text-align: center;
+  align-items: center;
+  color: #1c1c1c;
+`;
 
 const TextWrapper = styled.View`
-flex:1;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 type Props = {
   product: Product;
   elInterest: string;
-}
+};
 
 const InterestSummary: FunctionComponent<Props> = (props: Props) => {
   return (
@@ -60,24 +61,60 @@ const InterestSummary: FunctionComponent<Props> = (props: Props) => {
       <GrayBox>
         <WhiteBox>
           <TextWrapper>
-            <GrayText style={{ flex: 1, textAlign: 'left' }} > FROM </GrayText>
-            <GrayText style={{ flex: 1 }}> TO </GrayText>
-            <GrayText style={{ flex: 3, textAlign: 'right' }}> VALUE </GrayText>
+            <GrayText
+              allowFontScaling={false}
+              style={{ flex: 1, textAlign: 'left' }}>
+              FROM
+            </GrayText>
+            <GrayText allowFontScaling={false} style={{ flex: 1 }}>
+              TO
+            </GrayText>
+            <GrayText
+              allowFontScaling={false}
+              style={{ flex: 3, textAlign: 'right' }}>
+              VALUE
+            </GrayText>
           </TextWrapper>
           <View style={{ flex: 1 }}>
-            <View style={{ position: 'relative', top: '50%', width: "100%", height: 1, backgroundColor: "#E5E5E5" }}></View>
+            <View
+              style={{
+                position: 'relative',
+                top: '50%',
+                width: '100%',
+                height: 1,
+                backgroundColor: '#E5E5E5',
+              }}></View>
           </View>
           <TextWrapper>
-            <BlackText style={{ flex: 1, textAlign: 'left' }}> ELYSIA </BlackText>
-            <BlackText style={{ flex: 1 }}> YOU </BlackText>
-            <View style={{ flexDirection: "row", flex: 3, justifyContent: "flex-end" }}>
-              <BlackText style={{ textAlign: "right" }}>EL</BlackText>
-              <BlackText style={{ fontWeight: 'bold', textAlign: "right" }}> {props.elInterest} </BlackText>
+            <BlackText
+              allowFontScaling={false}
+              style={{ flex: 1, textAlign: 'left' }}>
+              ELYSIA
+            </BlackText>
+            <BlackText allowFontScaling={false} style={{ flex: 1 }}>
+              YOU
+            </BlackText>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 3,
+                justifyContent: 'flex-end',
+              }}>
+              <BlackText
+                allowFontScaling={false}
+                style={{ textAlign: 'right' }}>
+                EL
+              </BlackText>
+              <BlackText
+                allowFontScaling={false}
+                style={{ fontWeight: 'bold', textAlign: 'right' }}>
+                {props.elInterest}
+              </BlackText>
             </View>
           </TextWrapper>
         </WhiteBox>
       </GrayBox>
-    </View >
+    </View>
   );
 };
 
