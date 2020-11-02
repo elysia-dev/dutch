4d8293d363554de17a457dc06d82ca3e8c7e923d
hyemmie <lhm0710@naver.com>
2020-11-02 19:44:44 +0900

    fix default language issue

--- src/modules/more/Setting.tsx
+++ src/modules/more/Setting.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, useState, useEffect, useContext } from 'react';
+import React, {
+  FunctionComponent,
+  useState,
+  useEffect,
+  useContext,
+} from 'react';
 import { View, StyleSheet, Switch, Platform } from 'react-native';
 import { Picker } from '@react-native-community/picker';
 import * as Notifications from 'expo-notifications';
@@ -30,8 +35,9 @@ const Setting: FunctionComponent<Props> = (props: Props) => {
     Notifications.getPermissionsAsync().then((settings) => {
       AsyncStorage.getItem('pushNotificationPermission').then((permission) => {
         if (
-          settings.granted
-          || settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+          settings.granted ||
+          settings.ios?.status ===
+            Notifications.IosAuthorizationStatus.PROVISIONAL
         ) {
           AsyncStorage.getItem('pushNotificationPermission').then((res) => {
             setState({ ...state, hasPermission: res === 'granted' });
@@ -57,31 +63,7 @@ const Setting: FunctionComponent<Props> = (props: Props) => {
 
   const changeI18n = async (value: string) => {
     i18n.locale = value;
-    // await AsyncStorage.setItem('i18nLanguage', value);
   };
-
-  const TermListIos = [
-    {
-      label: "한국어",
-      value: 'ko',
-    },
-    {
-      label: "English",
-      value: 'en',
-    },
-    {
-      label: "简体中文",
-      value: 'zhHans',
-    },
-  ];
-
-  // eslint-disable-next-line no-nested-ternary
-  const currentTermNum = i18n.currentLocale() === LocaleType.KO ?
-    0 : i18n.currentLocale() === LocaleType.EN ? 1 : 2;
-
-  const currentTermListIos = [TermListIos[currentTermNum]]
-    .concat(TermListIos.filter(term => term.value !== i18n.currentLocale()));
-
 
   return (
     <WrapperLayout
@@ -99,12 +81,14 @@ const Setting: FunctionComponent<Props> = (props: Props) => {
               borderBottomColor: '#F6F6F8',
               top: 22,
             }}>
-            <H3Text label={i18n.t('more_label.notification_setting')}
+            <H3Text
+              label={i18n.t('more_label.notification_setting')}
               style={{
                 paddingLeft: '5%',
                 paddingRight: '5%',
                 fontSize: 18,
-              }} />
+              }}
+            />
             <View
               style={{
                 paddingTop: 30,
@@ -113,13 +97,14 @@ const Setting: FunctionComponent<Props> = (props: Props) => {
                 borderBottomWidth: 5,
                 borderBottomColor: '#F6F6F8',
               }}>
-              <View style={{
-                paddingLeft: '5%',
-                paddingRight: '5%',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignContent: 'center',
-              }}>
+              <View
+                style={{
+                  paddingLeft: '5%',
+                  paddingRight: '5%',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignContent: 'center',
+                }}>
                 <P1Text
                   label={i18n.t('more_label.push_notice')}
                   style={{
@@ -138,31 +123,42 @@ const Setting: FunctionComponent<Props> = (props: Props) => {
                 />
               </View>
             </View>
-            <H3Text label={i18n.t('more_label.language')}
+            <H3Text
+              label={i18n.t('more_label.language')}
               style={{
                 paddingLeft: '5%',
                 fontSize: 18,
-              }} />
-            <View style={{
-              marginLeft: '5%',
-              marginRight: '5%',
-            }}>
+              }}
+            />
+            <View
+              style={{
+                marginVertical: 20,
+                marginHorizontal: '5%',
+                borderWidth: 1,
+                borderRadius: 5,
+                borderColor: '#D0D8DF',
+                paddingRight: Platform.OS === 'android' ? 10 : 0,
+              }}>
               <Picker
                 style={{
-                  height: 200,
+                  height: Platform.OS === 'ios' ? 200 : 40,
+                  marginVertical: 10,
                 }}
                 accessibilityLabel={'settings'}
-                selectedValue={state.selectedValue}
+                selectedValue={i18n.currentLocale()}
                 onValueChange={async (itemValue) => {
                   changeI18n(itemValue.toString());
                   setState({ ...state, selectedValue: i18n.currentLocale() });
-                  await Server.resetLanguage(i18n.currentLocale()).catch(e => { alert(i18n.t('account_errors.server')); });
+                  await Server.resetLanguage(i18n.currentLocale()).catch(
+                    (e) => {
+                      alert(i18n.t('account_errors.server'));
+                    },
+                  );
                   changeLanguage(i18n.currentLocale() as LocaleType);
-                }}
-              >
-                <Picker.Item label={"한국어"} value="ko" key={0} />
-                <Picker.Item label={"English"} value="en" key={1} />
-                <Picker.Item label={"简体中文"} value="zhHans" key={2} />
+                }}>
+                <Picker.Item label={'한국어'} value="ko" key={0} />
+                <Picker.Item label={'English'} value="en" key={1} />
+                <Picker.Item label={'简体中文'} value="zhHans" key={2} />
               </Picker>
             </View>
             <View
