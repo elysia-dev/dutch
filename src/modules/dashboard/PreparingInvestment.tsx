c3c8c9867c192798ff4ea85135f9a654079d6def
hyemmie <lhm0710@naver.com>
2020-11-03 15:35:32 +0900

    add disable

--- src/modules/dashboard/PreparingInvestment.tsx
+++ src/modules/dashboard/PreparingInvestment.tsx
@@ -1,10 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { FunctionComponent, useContext } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import ConfettiCannon from 'react-native-confetti-cannon';
 import { BackButton } from '../../shared/components/BackButton';
 import { SubmitButton } from '../../shared/components/SubmitButton';
@@ -26,25 +22,27 @@ const StatusButton: FunctionComponent<ButtonProps> = (props: ButtonProps) => {
   return (
     <TouchableOpacity
       onPress={props.onPress}
+      disabled={props.color}
       style={{
-        width: "100%",
+        width: '100%',
         height: 50,
         padding: 15,
         marginBottom: 20,
         borderRadius: 5,
         borderWidth: 1,
-        borderColor: props.color ? "#3679B5" : "#D0D8DF",
-      }}
-    >
+        borderColor: props.color ? '#3679B5' : '#D0D8DF',
+      }}>
       <Text
         allowFontScaling={false}
         style={{
-          color: props.color ? "#1C1C1C" : "#A7A7A7",
+          color: props.color ? '#1C1C1C' : '#A7A7A7',
           fontSize: 15,
-          fontFamily: props.color ? "Roboto_700Bold" : "Roboto_400Regular",
+          fontFamily: props.color ? 'Roboto_700Bold' : 'Roboto_400Regular',
           alignItems: 'center',
-          }}>{props.title}</Text>
-    </TouchableOpacity >
+        }}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
@@ -53,54 +51,79 @@ const PreparingInvestment: FunctionComponent = () => {
   const { kycStatus, ethAddresses } = useContext(RootContext).user;
 
   return (
-    <View style={{ backgroundColor: "#fff", width: '100%', height: "100%", padding: 20 }}>
+    <View
+      style={{
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '100%',
+        padding: 20,
+      }}>
       <BackButton
         style={{ marginTop: 10 }}
         handler={() => navigation.goBack()}
       />
-      { (kycStatus === KycStatus.SUCCESS && ethAddresses !== null)
-        &&
+      {kycStatus === KycStatus.SUCCESS && ethAddresses !== null && (
         <ConfettiCannon
           count={100}
           origin={{ x: -10, y: -10 }}
           fallSpeed={3000}
           fadeOut={true}
         />
-      }
+      )}
       <TitleText label={i18n.t('dashboard.prepare_investment')} />
-      <SubTitleText label={i18n.t('dashboard.need_kyc_wallet')} style={{ marginTop: 10 }} />
-      <View style={{ marginTop: 30, width: "100%" }}>
+      <SubTitleText
+        label={i18n.t('dashboard.need_kyc_wallet')}
+        style={{ marginTop: 10 }}
+      />
+      <View style={{ marginTop: 30, width: '100%' }}>
         <StatusButton
           title={`${i18n.t(`more_label.${kycStatus}_kyc`)}`}
-          color={(kycStatus === KycStatus.PENDING || kycStatus === KycStatus.SUCCESS)}
+          color={
+            kycStatus === KycStatus.PENDING || kycStatus === KycStatus.SUCCESS
+          }
           onPress={() => {
             if ([KycStatus.REJECTED, KycStatus.NONE].includes(kycStatus)) {
-              navigation.navigate("Kyc", { screen: KycPage.StartKYC });
+              navigation.navigate('Kyc', { screen: KycPage.StartKYC });
             }
           }}
         />
         <StatusButton
-          title={ethAddresses?.length > 0 ? i18n.t('dashboard.wallet_connected') : i18n.t('dashboard.no_wallet')}
+          title={
+            ethAddresses?.length > 0
+              ? i18n.t('dashboard.wallet_connected')
+              : i18n.t('dashboard.no_wallet')
+          }
           color={ethAddresses?.length > 0}
           onPress={() => {
             if (!(ethAddresses?.length > 0)) {
-              navigation.navigate("More", { screen: MorePage.RegisterEthAddress });
+              navigation.navigate('More', {
+                screen: MorePage.RegisterEthAddress,
+              });
             }
           }}
         />
       </View>
 
-      <SubmitButton title={
-        (kycStatus === KycStatus.SUCCESS && ethAddresses !== null) ?
-          i18n.t('dashboard.event_guide_EL') : i18n.t('dashboard.complete_prepare')}
-        handler={() => { }}
+      <SubmitButton
+        disabled={!(kycStatus === KycStatus.SUCCESS && ethAddresses !== null)}
+        title={
+          kycStatus === KycStatus.SUCCESS && ethAddresses !== null
+            ? i18n.t('dashboard.event_guide_EL')
+            : i18n.t('dashboard.complete_prepare')
+        }
+        handler={() => {}}
         style={{
           width: '100%',
-          position: "absolute",
+          position: 'absolute',
           bottom: 20,
-          backgroundColor: (kycStatus === KycStatus.SUCCESS && ethAddresses !== null) ? "#3679B5" : "#D0D8DF",
-        }} />
-    </View>);
+          backgroundColor:
+            kycStatus === KycStatus.SUCCESS && ethAddresses !== null
+              ? '#3679B5'
+              : '#D0D8DF',
+        }}
+      />
+    </View>
+  );
 };
 
 export default PreparingInvestment;
