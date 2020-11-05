443fa4e6d3e035e731adbe4bba8442da8ae209e6
donguk.seo <donguk.seo@modori.io>
2020-11-05 14:49:08 +0900

    Modify dev api url

--- src/utiles/getEnvironment.ts
+++ src/utiles/getEnvironment.ts
@@ -34,8 +34,7 @@ function getEnvironment(): Environment {
 
   return {
     envName: "DEVELOPMENT",
-    // apiUrl: `http://${(manifest.debuggerHost || 'localhost').split(':').shift()}:3000`,
-    apiUrl: "https://staging-api.elysia.land",
+    apiUrl: `http://${(manifest.debuggerHost || 'localhost').split(':').shift()}:3000`,
     pusherAppKey: 'f8163691d5d47ddcfed7',
     // dappUrl: "localhost:5000",
     dappUrl: "staging-dapp.elysia.land",
