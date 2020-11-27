3e0df683a76cafceb260f549c430ef8cbbddd927
donguk.seo <donguk.seo@modori.io>
2020-11-27 12:22:41 +0900

    Update version

--- src/utiles/getEnvironment.ts
+++ src/utiles/getEnvironment.ts
@@ -17,7 +17,7 @@ function getEnvironment(): Environment {
       envName: 'PRODUCTION',
       apiUrl: 'https://api.elysia.land',
       dappUrl: 'dapp.elysia.land',
-      version: '1.1.0',
+      version: '1.1.1',
     };
   }
 
@@ -27,7 +27,7 @@ function getEnvironment(): Environment {
       envName: 'STAGING',
       apiUrl: 'https://staging-api.elysia.land',
       dappUrl: 'staging-dapp.elysia.land',
-      version: '1.2.0',
+      version: '1.1.1',
     };
   }
 
