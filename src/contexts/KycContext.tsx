308bb8f99e2eeec4ad16afb758906d82ea77dab3
hyemmie <lhm0710@naver.com>
2020-12-14 20:00:44 +0900

    update kyc context

--- src/contexts/KycContext.tsx
+++ src/contexts/KycContext.tsx
@@ -4,27 +4,19 @@ import * as ImageManipulator from 'expo-image-manipulator';
 type KycContextType = {
   idType: string;
   idPhoto: ImageManipulator.ImageResult;
-  hashedIdPhoto: string;
   selfie: ImageManipulator.ImageResult;
-  hashedSelfie: string;
   setIdType: (type: string) => void;
   setIdPhoto: (photo: ImageManipulator.ImageResult) => void;
-  setHashedIdPhoto: (hash: string) => void;
   setSelfie: (photo: ImageManipulator.ImageResult) => void;
-  setHashedSelfie: (hash: string) => void;
 };
 
 const KycContext = createContext<KycContextType>({
   idType: '',
   idPhoto: {} as ImageManipulator.ImageResult,
-  hashedIdPhoto: '',
   selfie: {} as ImageManipulator.ImageResult,
-  hashedSelfie: '',
   setIdType: (_type: string) => {},
   setIdPhoto: (_photo: ImageManipulator.ImageResult) => {},
-  setHashedIdPhoto: (_hash: string) => {},
   setSelfie: (_photo: ImageManipulator.ImageResult) => {},
-  setHashedSelfie: (_hash: string) => {},
 });
 
 export default KycContext;
