2efdc4b9a7fb9f9ebf8a4b7c5d36c1d344bf61f0
hyemmie <lhm0710@naver.com>
2021-01-13 17:16:30 +0900

    add: withdrawnMember page

---
+++ src/modules/account/WithdrawnMember.tsx
@@ -0,0 +1,59 @@
+import React, { FunctionComponent, useContext, useState } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+import i18n from '../../i18n/i18n';
+import { AccountPage } from '../../enums/pageEnum';
+import RootContext from '../../contexts/RootContext';
+import AccountLayout from '../../shared/components/AccountLayout';
+import { H1Text, H3Text, P1Text } from '../../shared/components/Texts';
+
+const WithdrawnMember: FunctionComponent = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={{ width: '100%', height: '100%', backgroundColor: '#3679B5' }}>
+      <Image
+        style={{
+          width: 120,
+          height: 120,
+          top: 150,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginBottom: 50,
+        }}
+        source={require('./images/elysialogo.png')}></Image>
+      <P1Text
+        style={{
+          position: 'relative',
+          top: 100,
+          color: '#fff',
+          textAlign: 'center',
+        }}
+        label={i18n.t('account.withdrawl_complete')}
+      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate(AccountPage.InitializeEmail)}
+        style={{
+          position: 'absolute',
+          bottom: '20%',
+          alignSelf: 'center',
+          backgroundColor: '#2C6190',
+          width: 200,
+          height: 50,
+          justifyContent: 'center',
+          alignContent: 'center',
+          borderRadius: 5,
+        }}>
+        <H3Text
+          style={{
+            color: '#fff',
+            fontSize: 16,
+            textAlign: 'center',
+          }}
+          label={i18n.t('account_label.move')}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default WithdrawnMember;
