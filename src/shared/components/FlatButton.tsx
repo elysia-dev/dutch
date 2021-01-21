2fb2b49ff731f47eefd81d3737b93b97dffcb313
hyemmie <lhm0710@naver.com>
2021-01-21 14:19:40 +0900

    add: style props

--- src/shared/components/FlatButton.tsx
+++ src/shared/components/FlatButton.tsx
@@ -1,22 +1,28 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent } from 'react';
 import {
   GestureResponderEvent,
+  StyleProp,
   TouchableOpacity,
-} from "react-native";
+  ViewStyle,
+} from 'react-native';
 import { P3Text } from './Texts';
 
 export const FlatButton: FunctionComponent<{
   title: string;
   handler: (event: GestureResponderEvent) => void;
-}> = ({ title, handler }) => {
+  style?: StyleProp<ViewStyle>;
+}> = ({ title, handler, style }) => {
   return (
-    <TouchableOpacity onPress={handler}>
-      <P3Text label={title} style={{
-        color: "#1c1c1c",
-        fontSize: 13,
-        lineHeight: 15,
-        textAlign: "center",
-      }}/>
+    <TouchableOpacity onPress={handler} style={style}>
+      <P3Text
+        label={title}
+        style={{
+          color: '#1c1c1c',
+          fontSize: 13,
+          lineHeight: 15,
+          textAlign: 'center',
+        }}
+      />
     </TouchableOpacity>
   );
 };
