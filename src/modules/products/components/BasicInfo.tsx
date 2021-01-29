1b9311eb50e0968acd80ff4c5246ba7a42acf92d
hyemmie <lhm0710@naver.com>
2021-01-29 19:35:27 +0900

    modify: product info view margin

--- src/modules/products/components/BasicInfo.tsx
+++ src/modules/products/components/BasicInfo.tsx
@@ -95,7 +95,6 @@ const BasicInfo: FunctionComponent<Props> = (props: Props) => {
         style={{
           marginTop: 25,
           width: '100%',
-          height: 157,
           backgroundColor: '#F6F6F8',
           borderRadius: 10,
           borderWidth: 1,
@@ -144,6 +143,7 @@ const BasicInfo: FunctionComponent<Props> = (props: Props) => {
           {product.financeType === 'loan' && (
             <View
               style={{
+                marginVertical: 2,
                 flex: 1,
                 flexDirection: 'row',
                 justifyContent: 'space-between',
@@ -165,6 +165,7 @@ const BasicInfo: FunctionComponent<Props> = (props: Props) => {
           )}
           <View
             style={{
+              marginVertical: 2,
               flex: 1,
               flexDirection: 'row',
               justifyContent: 'space-between',
@@ -190,6 +191,7 @@ const BasicInfo: FunctionComponent<Props> = (props: Props) => {
           </View>
           <View
             style={{
+              marginVertical: 2,
               flex: 1,
               flexDirection: 'row',
               justifyContent: 'space-between',
@@ -241,6 +243,7 @@ const BasicInfo: FunctionComponent<Props> = (props: Props) => {
           {hasChildProduct && (
             <View
               style={{
+                marginVertical: 2,
                 flex: 1,
                 flexDirection: 'row',
                 justifyContent: 'space-between',
