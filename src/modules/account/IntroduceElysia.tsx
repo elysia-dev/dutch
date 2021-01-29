642c2f8167bae48fcabdcc63d4250796bd8d600c
hyemmie <lhm0710@naver.com>
2021-01-29 11:53:08 +0900

    modify: login animation dot position

--- src/modules/account/IntroduceElysia.tsx
+++ src/modules/account/IntroduceElysia.tsx
@@ -115,151 +115,155 @@ const IntroduceElysia: FunctionComponent<{}> = () => {
     });
   return (
     <SafeAreaView
-      forceInset={{ top: 'always' }}
+      forceInset={{ top: 'always', bottom: 'always' }}
       style={{
         flex: 1,
         backgroundColor: '#FAFCFF',
-        paddingTop: 40,
         width: '100%',
         height: '100%',
       }}>
-      <Animated.View
-        style={{
-          position: 'absolute',
-          zIndex: 5,
-          backgroundColor: '#3679B5',
-          bottom: scrollX.interpolate({
-            inputRange: [-1, 0, 1, 2, 2.3, 2.5, 2.7, 3, 3.3, 3.4, 3.6, 4, 5],
-            outputRange: [
-              Dimensions.get('window').height * 0.5 + 10,
-              Dimensions.get('window').height * 0.5 + 10,
-              Dimensions.get('window').height * 0.5 + 60,
-              Dimensions.get('window').height * 0.5 + 85,
-              Dimensions.get('window').height * 0.5 + 180,
-              Dimensions.get('window').height * 0.5 + 160,
-              Dimensions.get('window').height * 0.5 + 180,
-              Dimensions.get('window').height * 0.5 + 120,
-              Dimensions.get('window').height * 0.5 + 100,
-              Dimensions.get('window').height * 0.5 + 90,
-              Dimensions.get('window').height * 0.5 + 90,
-              Dimensions.get('window').height * 0.5 + 85,
-              Dimensions.get('window').height * 0.5 + 85,
-            ],
-          }),
-          left: scrollX.interpolate({
-            inputRange: [-1, 0, 1, 2, 2.3, 2.5, 2.7, 3, 3.3, 3.4, 3.6, 4, 5],
-            outputRange: [
-              550,
-              Dimensions.get('window').width * 0.46 +
-                // eslint-disable-next-line no-nested-ternary
-                (i18n.currentLocale() === LocaleType.KO
-                  ? 0
-                  : i18n.currentLocale() === LocaleType.CH
-                  ? -60
-                  : 20),
-              Platform.OS === 'ios'
-                ? Dimensions.get('window').width * 0.55
-                : Dimensions.get('window').width * 0.433,
-              Dimensions.get('window').width * 0.28,
-              Dimensions.get('window').width * 0.92,
-              Dimensions.get('window').width * +20,
-              Dimensions.get('window').width,
-              Dimensions.get('window').width * 0.5,
-              -30,
-              -30,
-              Dimensions.get('window').width * 0.2,
-              Dimensions.get('window').width * 0.65,
-              -100,
-            ],
-          }),
-          width: scrollX.interpolate({
-            inputRange: [
-              -1,
-              0,
-              1,
-              2,
-              2.3,
-              2.301,
-              2.499,
-              2.5,
-              3,
-              3.3,
-              3.301,
-              3.599,
-              3.6,
-              4,
-              5,
-            ],
-            outputRange: [8, 8, 50, 30, 8, 0, 0, 8, 30, 8, 0, 0, 8, 30, 30],
-          }),
-          height: scrollX.interpolate({
-            inputRange: [
-              -1,
-              0,
-              1,
-              2,
-              2.3,
-              2.301,
-              2.499,
-              2.5,
-              3,
-              3.3,
-              3.301,
-              3.599,
-              3.6,
-              4,
-              5,
-            ],
-            outputRange: [8, 8, 50, 30, 8, 0, 0, 8, 30, 8, 0, 0, 8, 30, 30],
-          }),
-          opacity: scrollX.interpolate({
-            inputRange: [-1, 0, 1, 2, 2.7, 3, 4, 5],
-            outputRange: [1, 1, 0.8, 0.8, 1, 0.8, 0.8, 0.8],
-          }),
-        }}></Animated.View>
-      <ViewPager
-        style={{ width: '100%', height: '100%' }}
-        initialPage={0}
-        ref={viewPager}
-        onPageScroll={(e) => {
-          setScrollX(
-            new Animated.Value(e.nativeEvent.position + e.nativeEvent.offset),
-          );
-        }}
-        onPageSelected={(e) => {
-          setState(e.nativeEvent.position);
-        }}>
-        {ItemListing}
-      </ViewPager>
       <View
         style={{
-          alignItems: 'center',
-          alignSelf: 'center',
-          justifyContent: 'center',
-          flexDirection: 'row',
-          position: 'absolute',
-          top: 40 + Dimensions.get('window').height * 0.5,
-          // bottom: Dimensions.get('window').height * 0.4,
+          width: '100%',
+          height: '100%',
         }}>
-        {ButtonListing}
-      </View>
-      <SubmitButton
-        title={
-          i18n.t('account_label.login') + '/' + i18n.t('account_label.signup')
-        }
-        style={{
-          width: '90%',
-          marginHorizontal: '5%',
-          position: 'absolute',
-          bottom: 40,
-        }}
-        handler={() => navigation.navigate(AccountPage.InitializeEmail)}
-      />
-      {/* <FlatButton
+        <Animated.View
+          style={{
+            position: 'absolute',
+            zIndex: 5,
+            backgroundColor: '#3679B5',
+            bottom: scrollX.interpolate({
+              inputRange: [-1, 0, 1, 2, 2.3, 2.5, 2.7, 3, 3.3, 3.4, 3.6, 4, 5],
+              outputRange: [
+                Dimensions.get('window').height * 0.5 + 10,
+                Dimensions.get('window').height * 0.5 + 10,
+                Dimensions.get('window').height * 0.5 + 60,
+                Dimensions.get('window').height * 0.5 + 70,
+                Dimensions.get('window').height * 0.5 + 180,
+                Dimensions.get('window').height * 0.5 + 160,
+                Dimensions.get('window').height * 0.5 + 180,
+                Dimensions.get('window').height * 0.5 + 120,
+                Dimensions.get('window').height * 0.5 + 100,
+                Dimensions.get('window').height * 0.5 + 90,
+                Dimensions.get('window').height * 0.5 + 90,
+                Dimensions.get('window').height * 0.5 + 70,
+                Dimensions.get('window').height * 0.5 + 70,
+              ],
+            }),
+            left: scrollX.interpolate({
+              inputRange: [-1, 0, 1, 2, 2.3, 2.5, 2.7, 3, 3.3, 3.4, 3.6, 4, 5],
+              outputRange: [
+                550,
+                Dimensions.get('window').width * 0.46 +
+                  // eslint-disable-next-line no-nested-ternary
+                  (i18n.currentLocale() === LocaleType.KO
+                    ? 0
+                    : i18n.currentLocale() === LocaleType.CH
+                    ? -60
+                    : 20),
+                Platform.OS === 'ios'
+                  ? Dimensions.get('window').width * 0.55
+                  : Dimensions.get('window').width * 0.433,
+                Dimensions.get('window').width * 0.25,
+                Dimensions.get('window').width * 0.92,
+                Dimensions.get('window').width * +20,
+                Dimensions.get('window').width,
+                Dimensions.get('window').width * 0.5,
+                -30,
+                -30,
+                Dimensions.get('window').width * 0.2,
+                Dimensions.get('window').width * 0.68,
+                -100,
+              ],
+            }),
+            width: scrollX.interpolate({
+              inputRange: [
+                -1,
+                0,
+                1,
+                2,
+                2.3,
+                2.301,
+                2.499,
+                2.5,
+                3,
+                3.3,
+                3.301,
+                3.599,
+                3.6,
+                4,
+                5,
+              ],
+              outputRange: [8, 8, 50, 30, 8, 0, 0, 8, 30, 8, 0, 0, 8, 30, 30],
+            }),
+            height: scrollX.interpolate({
+              inputRange: [
+                -1,
+                0,
+                1,
+                2,
+                2.3,
+                2.301,
+                2.499,
+                2.5,
+                3,
+                3.3,
+                3.301,
+                3.599,
+                3.6,
+                4,
+                5,
+              ],
+              outputRange: [8, 8, 50, 30, 8, 0, 0, 8, 30, 8, 0, 0, 8, 30, 30],
+            }),
+            opacity: scrollX.interpolate({
+              inputRange: [-1, 0, 1, 2, 2.7, 3, 4, 5],
+              outputRange: [1, 1, 0.8, 0.8, 1, 0.8, 0.8, 0.8],
+            }),
+          }}></Animated.View>
+        <ViewPager
+          style={{ width: '100%', height: '100%' }}
+          initialPage={0}
+          ref={viewPager}
+          onPageScroll={(e) => {
+            setScrollX(
+              new Animated.Value(e.nativeEvent.position + e.nativeEvent.offset),
+            );
+          }}
+          onPageSelected={(e) => {
+            setState(e.nativeEvent.position);
+          }}>
+          {ItemListing}
+        </ViewPager>
+        <View
+          style={{
+            alignItems: 'center',
+            alignSelf: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row',
+            position: 'absolute',
+            bottom: Dimensions.get('window').height * 0.4,
+          }}>
+          {ButtonListing}
+        </View>
+        <SubmitButton
+          title={
+            i18n.t('account_label.login') + '/' + i18n.t('account_label.signup')
+          }
+          style={{
+            width: '90%',
+            marginHorizontal: '5%',
+            position: 'absolute',
+            bottom: 20,
+          }}
+          handler={() => navigation.navigate(AccountPage.InitializeEmail)}
+        />
+        {/* <FlatButton
         style={{ position: 'absolute', bottom: 65, alignSelf: 'center' }}
         title={i18n.t('account.forget_password_link')}
         handler={() => navigation.navigate(AccountPage.InitializeEmail)}
       /> */}
+      </View>
     </SafeAreaView>
   );
 };
