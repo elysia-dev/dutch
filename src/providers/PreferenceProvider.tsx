78c6768f3b0c3a7475f060abd4eb0d91a99c8888
donguk.seo <donguk.seo@modori.io>
2021-04-28 10:27:19 +0900

    Update expo version : 40 -> 41

--- src/providers/PreferenceProvider.tsx
+++ src/providers/PreferenceProvider.tsx
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useCallback, useEffect } from 'react';
 import { useState } from "react";
 import { CURRENCY, LANGUAGE, NOTIFICATION } from '../constants/storage';
