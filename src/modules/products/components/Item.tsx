f1fba879c733522944272163d2aa4f04cbdf65f7
hyunoh-modori <hyunoh.bae@modori.io>
2021-05-13 16:17:39 +0900

    change item listing & add enums (temp)

--- src/modules/products/components/Item.tsx
+++ src/modules/products/components/Item.tsx
@@ -1,9 +1,15 @@
-import React, { createRef, FunctionComponent, useState } from 'react';
+import React, { createRef, FunctionComponent, useEffect, useState } from 'react';
 import { View, Image, Animated, TouchableWithoutFeedback } from 'react-native';
+import AssetType from '../../../enums/AssetType';
+import PaymentCryptoType from '../../../enums/PaymentCryptoType';
 import CachedImage from '../../../shared/components/CachedImage';
 
-import { H1Text, H2Text, P1Text } from '../../../shared/components/Texts';
+import { H1Text, H2Text, P1Text, P2Text } from '../../../shared/components/Texts';
 import { Story } from '../../../types/product';
+
+import EL from '../../../../src/shared/assets/images/el.png';
+import ETH from '../../../../src/shared/assets/images/eth.png';
+import BNB from '../../../../src/shared/assets/images/bnb.png';
 
 interface Props {
   story: Story;
@@ -13,6 +19,23 @@ interface Props {
 
 export const Item: FunctionComponent<Props> = (props: Props) => {
   const ref = createRef<Image>();
+
+  const setImage = (method: PaymentCryptoType) => {
+    switch(method) {
+      case PaymentCryptoType.EL :
+        return (<Image style={{ width: 17, height: 17, marginRight: 5 }} source={EL} />)
+      case PaymentCryptoType.ETH :
+        return (<Image style={{ width: 17, height: 17, marginRight: 5 }} source={ETH} />)
+      case PaymentCryptoType.BNB :
+        return (<Image style={{ width: 17, height: 17, marginRight: 5 }} source={BNB} />)
+      default:
+        break;
+    }
+  }
+  useEffect(() => {
+    console.log(props.story)
+    console.log(props.story.paymentMethod)
+  }, [])
 
   return (
     <TouchableWithoutFeedback
@@ -27,7 +50,7 @@ export const Item: FunctionComponent<Props> = (props: Props) => {
         style={{
           backgroundColor: '#fff',
           elevation: 3,
-          height: 416,
+          height: 516,
           marginLeft: 3,
           marginRight: 3,
           borderRadius: 10,
@@ -45,11 +68,48 @@ export const Item: FunctionComponent<Props> = (props: Props) => {
           source={{ uri: props.story.image }}
           style={{
             width: '100%',
-            height: '100%',
+            height: 416,
             resizeMode: 'cover',
             borderRadius: 10,
           }}
         />
+        <View style={{
+          marginHorizontal: "5%"
+        }}>
+          <View style={{
+            height: 50,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            borderBottomColor: "#F1F1F1",
+            borderBottomWidth: 1,
+            alignItems: "center"
+          }}>
+            <P1Text label={"투자방식"} style={{ fontSize: 15 }} />
+            <H2Text label={
+              props.story.investmentMethod === AssetType.Unit ?
+              "갯수 기준"
+              :
+              "원화 기준"
+            } style={{ fontSize: 15 }} />
+          </View>
+          <View style={{
+            height: 50,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            borderBottomColor: "#F1F1F1",
+            borderBottomWidth: 1,
+            alignItems: "center"
+          }}>
+            <P1Text label={"투자가능수단"} style={{ fontSize: 15 }} />
+            <View style={{
+              flexDirection: "row",
+              alignItems: "center"
+            }}>
+              {setImage(props.story.paymentMethod)}
+              <H2Text label={props.story.paymentMethod.toUpperCase()} style={{ fontSize: 15 }} />
+            </View>
+          </View>
+        </View>
         <View
           style={{
             position: 'absolute',
