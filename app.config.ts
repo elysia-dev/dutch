3ee6e79c65b6b211445f73b82fa2296a87bffbcc
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-06-25 17:18:36 +0900

    Update version

--- app.config.ts
+++ app.config.ts
@@ -17,7 +17,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   privacy: 'hidden',
   name,
   slug,
-  version: '1.5.0',
+  version: '1.5.1',
   orientation: 'portrait',
   updates: {
     fallbackToCacheTimeout: 0,
@@ -88,4 +88,4 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   entryPoint: 'node_modules/expo/AppEntry.js',
   scheme: 'elysia-app',
-})
+});
