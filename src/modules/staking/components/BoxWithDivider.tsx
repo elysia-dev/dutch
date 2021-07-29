eefcdf6cb75b3c46b836279a97f6c28c34cbb56f
leegakyeong <alaska7132@naver.com>
2021-07-29 16:41:06 +0900

    Update BoxWithDivider

--- src/modules/staking/components/BoxWithDivider.tsx
+++ src/modules/staking/components/BoxWithDivider.tsx
@@ -4,7 +4,10 @@ import AppColors from '../../../enums/AppColors';
 import AppFonts from '../../../enums/AppFonts';
 
 const BoxWithDivider: React.FC<{
-  contents: { label: string; value: string }[];
+  contents: {
+    label: string | React.ReactNode;
+    value: string | React.ReactNode;
+  }[];
   boxStyle?: StyleProp<ViewStyle>;
   innerBoxStyle?: StyleProp<ViewStyle>;
   labelStyle?: StyleProp<TextStyle>;
@@ -13,6 +16,7 @@ const BoxWithDivider: React.FC<{
   return (
     <View
       style={{
+        backgroundColor: AppColors.WHITE,
         borderColor: AppColors.SUB_GREY,
         borderRadius: 5,
         borderWidth: 1,
@@ -23,33 +27,45 @@ const BoxWithDivider: React.FC<{
           <View
             key={String(content.label)}
             style={{
+              backgroundColor: AppColors.WHITE,
               flexDirection: 'row',
-              justifyContent: 'space-between',
+              justifyContent:
+                content.label && content.value ? 'space-between' : 'center',
               paddingVertical: 24,
               paddingHorizontal: 16,
               borderTopColor: AppColors.SUB_GREY,
               borderTopWidth: i === 0 ? 0 : 1,
               ...(innerBoxStyle as {}),
             }}>
-            <Text
-              style={{
-                color: AppColors.SUB_BLACK,
-                fontFamily: AppFonts.Regular,
-                fontSize: 14,
-                ...(labelStyle as {}),
-              }}>
-              {content.label}
-            </Text>
-            <Text
-              style={{
-                color: AppColors.BLACK,
-                fontFamily: AppFonts.Regular,
-                fontSize: 14,
-                textAlign: 'right',
-                ...(valueStyle as {}),
-              }}>
-              {content.value}
-            </Text>
+            {typeof content.label === 'string' ? (
+              <Text
+                style={{
+                  color: AppColors.SUB_BLACK,
+                  fontFamily: AppFonts.Regular,
+                  fontSize: 14,
+                  ...(labelStyle as {}),
+                }}>
+                {content.label}
+              </Text>
+            ) : (
+              <View style={{ flex: 1 }}>{content.label}</View>
+            )}
+            {typeof content.value === 'string' ? (
+              <Text
+                style={{
+                  color: AppColors.BLACK,
+                  fontFamily: AppFonts.Medium,
+                  fontSize: 14,
+                  textAlign: 'right',
+                  ...(valueStyle as {}),
+                }}>
+                {content.value}
+              </Text>
+            ) : (
+              <View style={{ flex: 1, flexDirection: 'row-reverse' }}>
+                {content.value}
+              </View>
+            )}
           </View>
         );
       })}
