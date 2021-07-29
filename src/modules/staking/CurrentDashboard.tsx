f5e5b49e440d30c9ba30ec06dbad36a960f72e39
leegakyeong <alaska7132@naver.com>
2021-07-29 12:50:19 +0900

    Update CurrentDashboard design - Add ScrollView

--- src/modules/staking/CurrentDashboard.tsx
+++ src/modules/staking/CurrentDashboard.tsx
@@ -1,18 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import SheetHeader from '../../shared/components/SheetHeader';
 import AppColors from '../../enums/AppColors';
+import NextButton from '../../shared/components/NextButton';
+import { Page, StakingPage } from '../../enums/pageEnum';
+import AppFonts from '../../enums/AppFonts';
+import { TitleText, SubTitleText } from '../../shared/components/Texts';
+import DotGraph from './components/DotGraph';
+import BarGraph from './components/BarGraph';
+import BoxWithDivider from './components/BoxWithDivider';
+import MiningPlan from './components/MiningPlan';
 
 const DashBoard: React.FC<{ route: any; navigation: any }> = ({ route }) => {
   const { type } = route.params;
+  const navigation = useNavigation();
+
   return (
     <View
       style={{
         backgroundColor: AppColors.WHITE,
         height: '100%',
+        width: '100%',
       }}>
-      <SheetHeader title="" />
-      <Text>{type}</Text>
+      <ScrollView
+        style={{
+          backgroundColor: AppColors.WHITE,
+          height: '100%',
+        }}>
+        <SheetHeader title="" />
+        <View style={{ paddingHorizontal: 20 }}>
+          <SubTitleText
+            label="보상으로 ELFI 수령하는"
+            style={{ fontSize: 14 }}
+          />
+          <TitleText label={`${type} 스테이킹`} style={{ fontSize: 22 }} />
+          <TitleText label="ELFI 채굴 플랜" style={{ fontSize: 22 }} />
+        </View>
+      </ScrollView>
+      <View></View>
+      <NextButton
+        title="1차 스테이킹"
+        handler={() => {
+          navigation.navigate(Page.Staking, {
+            screen: StakingPage.Stake,
+            params: { type },
+          });
+        }}
+        style={{
+          marginBottom: 20,
+          marginHorizontal: 16,
+        }}
+      />
     </View>
   );
 };
