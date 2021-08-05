78e0eb77181ff1e1eb37d9669b489df24f7dde58
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-05 14:30:36 +0900

    Modify paymentthod

--- src/modules/asset/components/TransactionList.tsx
+++ src/modules/asset/components/TransactionList.tsx
@@ -58,6 +58,7 @@ const TransactionList: React.FC<ITransactionList> = ({
               transaction={item}
               unit={unit}
               networkType={networkType}
+              paymentMethod={'NONE'}
             />
           );
         }}
