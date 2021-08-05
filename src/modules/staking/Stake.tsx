149f86b62860874464a2d9b21ba5715f05a6cba4
leegakyeong <alaska7132@naver.com>
2021-08-05 17:11:33 +0900

    Updste Stake and Unstake page to show PaymentSelection

--- src/modules/staking/Stake.tsx
+++ src/modules/staking/Stake.tsx
@@ -15,6 +15,12 @@ import PriceContext from '../../contexts/PriceContext';
 import AssetContext from '../../contexts/AssetContext';
 import decimalFormatter from '../../utiles/decimalFormatter';
 import calcAPR from '../../utiles/calculateAPR';
+import PaymentSelection from '../../shared/components/PaymentSelection';
+import {
+  getElStakingPoolContract,
+  getElfiStakingPoolContract,
+} from '../../utiles/getContract';
+import CryptoType from '../../enums/CryptoType';
 
 const Stake: React.FC<{ route: any }> = ({ route }) => {
   const { cryptoType, selectedRound } = route.params;
@@ -24,141 +30,161 @@ const Stake: React.FC<{ route: any }> = ({ route }) => {
   const insets = useSafeAreaInsets();
   const { getCryptoPrice } = useContext(PriceContext);
   const { getBalance } = useContext(AssetContext);
+  const [selectionVisible, setSelectionVisible] = useState(false);
+  const contract =
+    cryptoType === CryptoType.EL
+      ? getElStakingPoolContract()
+      : getElfiStakingPoolContract();
+
+  if (!selectionVisible) {
+    return (
+      <View style={{ backgroundColor: AppColors.WHITE, height: '100%' }}>
+        <SheetHeader title={`${cryptoType} 스테이킹`} />
+        <View
+          style={{
+            alignSelf: 'center',
+            borderColor: AppColors.SUB_GREY,
+            borderRadius: 5,
+            borderWidth: 1,
+            width: '90%',
+            height: 45,
+            padding: 12,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              color: AppColors.BLACK,
+              fontFamily: AppFonts.Bold,
+              fontSize: 14,
+            }}>
+            {`${selectedRound}차 스테이킹 APR`}
+          </Text>
+          <Text
+            style={{
+              color: AppColors.BLACK,
+              fontFamily: AppFonts.Bold,
+              fontSize: 14,
+            }}>
+            {`${calcAPR(cryptoType, selectedRound).toString()} %`}
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: Platform.OS === 'android' ? 20 : 10,
+            paddingHorizontal: 20,
+            flex: 1,
+          }}>
+          <LargeTextInput
+            placeholder="몇 개를 스테이킹할까요?"
+            value={value}
+            unit={cryptoType}
+          />
+          <InputInfoBox
+            list={[
+              `스테이킹 달러 가치: $ ${decimalFormatter(
+                parseFloat(value || '0') * getCryptoPrice(cryptoType),
+                6,
+              )}`,
+              `스테이킹 가능 수량: ${decimalFormatter(
+                getBalance(cryptoType),
+                6,
+              )} ${cryptoType}`,
+              `예상 가스비: ${'(모름)'}`,
+            ]}
+          />
+          <NumberPadShortcut
+            values={[0.01, 1, 10, 100, 1000]}
+            inputValue={value}
+            setValue={setValue}
+          />
+          <NumberPad
+            addValue={(text) => {
+              const includesComma = value.includes('.');
+              if (
+                (text === '.' && includesComma) ||
+                (text !== '.' && !includesComma && value.length >= 12) ||
+                (includesComma && value.split('.')[1].length >= 6) ||
+                (value
+                  .split('')
+                  .reduce((res, cur) => res && cur === '0', true) &&
+                  text === '0')
+              ) {
+                return;
+              }
+
+              const next =
+                text === '.' && !value
+                  ? '0.'
+                  : text !== '0' && value === '0'
+                  ? text
+                  : value + text;
+
+              setValue(next);
+            }}
+            removeValue={() => setValue(value.slice(0, -1))}
+          />
+        </View>
+        <View
+          style={{
+            marginBottom: insets.bottom || 10,
+            paddingLeft: '5%',
+            paddingRight: '5%',
+          }}>
+          <NextButton
+            title="입력 완료"
+            disabled={!value}
+            handler={() => {
+              if (isWalletUser) {
+                setModalVisible(true);
+              } else {
+                setSelectionVisible(true);
+              }
+            }}
+          />
+        </View>
+        <ConfirmationModal
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          title={`${cryptoType} 스테이킹`}
+          subtitle="최종 확인을 해 주세요!"
+          list={[
+            { label: `스테이킹 회차`, value: `${selectedRound}차 스테이킹` },
+            {
+              label: `스테이킹 수량`,
+              value: `${value} ${cryptoType}`,
+              subvalue: `$ ${decimalFormatter(
+                parseFloat(value || '0') * getCryptoPrice(cryptoType),
+                6,
+              )}`,
+            },
+            { label: '가스비', value: '(모름)' },
+          ]}
+          isApproved={true}
+          submitButtonText={`${selectedRound}차 스테이킹`}
+          handler={() => console.log('내부 지갑 계정 트랜잭션')}
+        />
+        {/* <OverlayLoading
+          visible={[
+            TxStep.Approving,
+            Platform.OS === 'android' && TxStep.CheckAllowance,
+            TxStep.Creating,
+          ].includes(step)}
+        /> */}
+      </View>
+    );
+  }
 
   return (
-    <View style={{ backgroundColor: AppColors.WHITE, height: '100%' }}>
-      <SheetHeader title={`${cryptoType} 스테이킹`} />
-      <View
-        style={{
-          alignSelf: 'center',
-          borderColor: AppColors.SUB_GREY,
-          borderRadius: 5,
-          borderWidth: 1,
-          width: '90%',
-          height: 45,
-          padding: 12,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
-        <Text
-          style={{
-            color: AppColors.BLACK,
-            fontFamily: AppFonts.Bold,
-            fontSize: 14,
-          }}>
-          {`${selectedRound}차 스테이킹 APR`}
-        </Text>
-        <Text
-          style={{
-            color: AppColors.BLACK,
-            fontFamily: AppFonts.Bold,
-            fontSize: 14,
-          }}>
-          {`${calcAPR(cryptoType, selectedRound).toString()} %`}
-        </Text>
-      </View>
-      <View
-        style={{
-          marginTop: Platform.OS === 'android' ? 20 : 10,
-          paddingHorizontal: 20,
-          flex: 1,
-        }}>
-        <LargeTextInput
-          placeholder="몇 개를 스테이킹할까요?"
-          value={value}
-          unit={cryptoType}
-        />
-        <InputInfoBox
-          list={[
-            `스테이킹 달러 가치: $ ${decimalFormatter(
-              parseFloat(value || '0') * getCryptoPrice(cryptoType),
-              6,
-            )}`,
-            `스테이킹 가능 수량: ${decimalFormatter(
-              getBalance(cryptoType),
-              6,
-            )} ${cryptoType}`,
-            `예상 가스비: ${'(모름)'}`,
-          ]}
-        />
-        <NumberPadShortcut
-          values={[0.01, 1, 10, 100, 1000]}
-          inputValue={value}
-          setValue={setValue}
-        />
-        <NumberPad
-          addValue={(text) => {
-            const includesComma = value.includes('.');
-            if (
-              (text === '.' && includesComma) ||
-              (text !== '.' && !includesComma && value.length >= 12) ||
-              (includesComma && value.split('.')[1].length >= 6) ||
-              (value.split('').reduce((res, cur) => res && cur === '0', true) &&
-                text === '0')
-            ) {
-              return;
-            }
-
-            const next =
-              text === '.' && !value
-                ? '0.'
-                : text !== '0' && value === '0'
-                ? text
-                : value + text;
-
-            setValue(next);
-          }}
-          removeValue={() => setValue(value.slice(0, -1))}
-        />
-      </View>
-      <View
-        style={{
-          marginBottom: insets.bottom || 10,
-          paddingLeft: '5%',
-          paddingRight: '5%',
-        }}>
-        <NextButton
-          title="입력 완료"
-          disabled={!value}
-          handler={() => {
-            if (isWalletUser) {
-              setModalVisible(true);
-            } else {
-              console.log('스테이킹 해야 함');
-            }
-          }}
-        />
-      </View>
-      <ConfirmationModal
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
-        title={`${cryptoType} 스테이킹`}
-        subtitle="최종 확인을 해 주세요!"
-        list={[
-          { label: `스테이킹 회차`, value: `${selectedRound}차 스테이킹` },
-          {
-            label: `스테이킹 수량`,
-            value: `${value} ${cryptoType}`,
-            subvalue: `$ ${decimalFormatter(
-              parseFloat(value || '0') * getCryptoPrice(cryptoType),
-              6,
-            )}`,
-          },
-          { label: '가스비', value: '(모름)' },
-        ]}
-        isApproved={true}
-        submitButtonText={`${selectedRound}차 스테이킹`}
-        handler={() => console.log('스테이킹 해야 함')}
-      />
-      {/* <OverlayLoading
-        visible={[
-          TxStep.Approving,
-          Platform.OS === 'android' && TxStep.CheckAllowance,
-          TxStep.Creating,
-        ].includes(step)}
-      /> */}
-    </View>
+    <PaymentSelection
+      value={parseFloat(value)}
+      page="staking"
+      stakingTxData={{
+        type: 'stake',
+      }}
+      contractAddress={contract?.address}
+    />
   );
 };
 
