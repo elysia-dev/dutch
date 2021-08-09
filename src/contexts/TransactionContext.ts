ab6d17891e3b3f430cde7696ecbe276ce80baa44
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-10 02:37:14 +0900

    Add function parameter

--- src/contexts/TransactionContext.ts
+++ src/contexts/TransactionContext.ts
@@ -1,25 +1,31 @@
+import { ethers } from 'ethers';
 import { createContext } from 'react';
 import CryptoTransaction from '../types/CryptoTransaction';
 
 export type TransactionType = {
-  transactions: CryptoTransaction[]
-  counter: number
+  transactions: CryptoTransaction[];
+  counter: number;
 };
 
 export interface ITransactionContext extends TransactionType {
-  addPendingTransaction: (tx: CryptoTransaction) => void
+  addPendingTransaction: (
+    txRes: ethers.providers.TransactionResponse | undefined,
+    tx: CryptoTransaction,
+  ) => void;
 }
 
 export const initialTransactions = {
   transactions: [],
-  counter: 0
-}
+  counter: 0,
+};
 
 export const initialTransactionContext = {
   ...initialTransactions,
-  addPendingTransaction: () => { },
-}
+  addPendingTransaction: () => {},
+};
 
-const TransactionContext = createContext<ITransactionContext>(initialTransactionContext);
+const TransactionContext = createContext<ITransactionContext>(
+  initialTransactionContext,
+);
 
 export default TransactionContext;
