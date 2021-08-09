e9b22d9d8d413c20b35be8fa4ac8f02a29f5f1f9
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-10 02:33:32 +0900

    Add display of purchased products pending transaction

--- src/modules/dashboard/components/AssetItem.tsx
+++ src/modules/dashboard/components/AssetItem.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { View, TouchableOpacity, Image } from 'react-native';
 import PreferenceContext from '../../../contexts/PreferenceContext';
 import PriceContext from '../../../contexts/PriceContext';
+import TransactionContext from '../../../contexts/TransactionContext';
 import CryptoType from '../../../enums/CryptoType';
 import CryptoImage from '../../../shared/components/CryptoImage';
 import { P1Text, P2Text } from '../../../shared/components/Texts';
@@ -16,45 +17,68 @@ interface IAssetItem {
 
 export const AssetItem: React.FC<IAssetItem> = ({
   asset,
-  onPress = () => { },
+  onPress = () => {},
   touchable = true,
 }) => {
   const { currencyFormatter } = useContext(PreferenceContext);
   const { getCryptoPrice } = useContext(PriceContext);
-
-
+  const { transactions } = useContext(TransactionContext);
   return (
     <TouchableOpacity
       onPress={() => onPress(asset)}
       disabled={!touchable}
-      style={{ display: 'flex', flexDirection: 'row', height: 60, paddingTop: 5, paddingBottom: 5, alignItems: 'center' }}
-    >
-      {asset.type === CryptoType.ELA
-      ?
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        height: 60,
+        paddingTop: 5,
+        paddingBottom: 5,
+        alignItems: 'center',
+      }}>
+      {asset.type === CryptoType.ELA ? (
         <>
           <CryptoImage
             type={asset.image!}
-            style={{ borderColor: "#F1F1F1", borderWidth: 1 }}
+            style={{ borderColor: '#F1F1F1', borderWidth: 1 }}
           />
           <CryptoImage
             type={asset.paymentMethod!}
-            style={{ width: 25, height: 25, position: "absolute", bottom: 5, left: 20 }}
+            style={{
+              width: 25,
+              height: 25,
+              position: 'absolute',
+              bottom: 5,
+              left: 20,
+            }}
           />
         </>
-      :
+      ) : (
         <CryptoImage type={asset.type} />
-      }
+      )}
       <View style={{ marginLeft: 15 }}>
         <P1Text label={asset.title} />
-        <P2Text label={`${asset.value >= 0.01 ? commaFormatter(Math.floor(asset.value * 100) / 100) : asset.value === 0 ? 0 : '0.00...'} ${asset.unit}`} />
-      </View>
-      <P1Text
-        style={{ marginLeft: 'auto' }}
-        label={currencyFormatter(
-          asset.value * getCryptoPrice(asset.type),
-          2,
+        {asset.value > 0 ? (
+          <P2Text
+            label={`${
+              asset.value >= 0.01
+                ? commaFormatter(Math.floor(asset.value * 100) / 100)
+                : asset.value === 0
+                ? 0
+                : '0.00...'
+            } ${asset.unit}`}
+          />
+        ) : (
+          <P2Text label={`0 ${asset.unit}`} />
         )}
-      />
+      </View>
+      {asset.type === CryptoType.ELA && asset.value <= 0 ? (
+        <P1Text style={{ marginLeft: 'auto' }} label={'거래대기중'} />
+      ) : (
+        <P1Text
+          style={{ marginLeft: 'auto' }}
+          label={currencyFormatter(asset.value * getCryptoPrice(asset.type), 2)}
+        />
+      )}
     </TouchableOpacity>
   );
 };
