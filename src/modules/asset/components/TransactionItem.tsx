85ed88bf474c31db5e004045857759c01f9ac33c
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-10 02:17:28 +0900

    Remove speedUp Function

--- src/modules/asset/components/TransactionItem.tsx
+++ src/modules/asset/components/TransactionItem.tsx
@@ -52,14 +52,12 @@ const TransactionItem: React.FC<ITransactionItem> = ({
   contractAddress,
   paymentMethod,
 }) => {
-  const { addPendingTransaction } = useContext(TransactionContext);
   const { currencyFormatter } = useContext(PreferenceContext);
   const { assets } = useContext(AssetContext);
   const { gasPrice, bscGasPrice, getCryptoPrice } = useContext(PriceContext);
   const { waitTxResult } = useContext(AssetContext);
   const { wallet } = useContext(WalletContext);
   const [crytoValue, setCrytoValue] = useState<Asset>();
-  const { afterTxFailed, afterTxHashCreated, afterTxCreated } = useTxHandler();
   const [isModalViasible, setIsModalViasible] = useState(false);
   const [updateGasPrice, setUpdateGasPrice] = useState('1');
   const [contract, setContract] = useState<Contract | null>();
@@ -68,70 +66,6 @@ const TransactionItem: React.FC<ITransactionItem> = ({
   const [isDisabled, setIsDisabled] = useState(false);
   const elContract = getElysiaContract();
   const { t } = useTranslation();
-
-  const speedUp = async () => {
-    try {
-      setIsDisabled(true);
-      let txRes: ethers.providers.TransactionResponse | undefined;
-      if (paymentMethod === 'NONE') {
-        txRes = await sendCryptoAsset(
-          gasPrice,
-          bscGasPrice,
-          transaction.cryptoType,
-          transaction.toAddress,
-          transaction.value,
-          wallet,
-          updateGasPrice,
-          Accelerate.Accelerate,
-        );
-      } else {
-        txRes = await purchaseProduct(
-          gasPrice,
-          bscGasPrice,
-          paymentMethod,
-          contract || null,
-          transaction.valueFrom || '',
-          wallet,
-          getCryptoPrice,
-          updateGasPrice,
-          Accelerate.Accelerate,
-        );
-      }
-      afterTxHashCreated(
-        wallet?.getFirstAddress() || '',
-        contractAddress || '',
-        txRes?.hash || '',
-        NetworkType.ETH,
-      );
-      const isCrytoELA = paymentMethod !== 'NONE';
-      addPendingTransaction({
-        txHash: txRes?.hash,
-        cryptoType: isCrytoELA ? CryptoType.ELA : transaction.cryptoType,
-        value: transaction.value,
-        createdAt: '',
-        type: isCrytoELA ? 'in' : 'out',
-        blockNumber: 0,
-        productId: isCrytoELA ? transaction.productId : undefined,
-        valueFrom: isCrytoELA ? transaction.valueFrom : undefined,
-        toAddress: isCrytoELA ? '' : transaction.toAddress,
-      });
-      setIsModalViasible((prev) => !prev);
-      const re = await txRes?.wait();
-      const date = await provider.getBlock(re?.blockNumber || '');
-      waitTxResult('', 3);
-      addPendingTransaction({
-        txHash: txRes?.hash,
-        cryptoType: isCrytoELA ? CryptoType.ELA : transaction.cryptoType,
-        value: transaction.value,
-        createdAt: moment.unix(date.timestamp).toString(),
-        type: isCrytoELA ? 'in' : 'out',
-        blockNumber: re?.blockNumber,
-        productId: isCrytoELA ? transaction.productId : undefined,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
   const getEstimateGas = async (text: string) => {
     let estimateGas: BigNumber | undefined;
@@ -326,22 +260,32 @@ const TransactionItem: React.FC<ITransactionItem> = ({
           }
         />
       </View>
-      <View>
-        <AccelerateModal
-          isModalViasible={isModalViasible}
-          setIsModalViasible={setIsModalViasible}
-          getEstimateGas={getEstimateGas}
-          updateGasPrice={updateGasPrice}
-          paymentMethod={paymentMethod}
-          valueInCryto={valueInCryto}
-          gasFee={gasFee}
-          crytoValue={crytoValue?.value}
-          speedUp={speedUp}
-          isDisabled={isDisabled}
-          setIsDisabled={setIsDisabled}
-          txCryptoType={transaction.cryptoType}
-        />
-      </View>
+      {isModalViasible && (
+        <View>
+          <AccelerateModal
+            isModalViasible={isModalViasible}
+            setIsModalViasible={setIsModalViasible}
+            getEstimateGas={getEstimateGas}
+            updateGasPrice={updateGasPrice}
+            paymentMethod={paymentMethod}
+            valueInCryto={valueInCryto}
+            gasFee={gasFee}
+            crytoValue={crytoValue?.value}
+            contract={contract}
+            isDisabled={isDisabled}
+            setIsDisabled={setIsDisabled}
+            txCryptoType={transaction.cryptoType}
+            productId={transaction.productId}
+            valueFrom={
+              paymentMethod === 'NONE'
+                ? transaction.value
+                : transaction.valueFrom
+            }
+            value={transaction.value}
+            address={transaction.toAddress}
+          />
+        </View>
+      )}
     </View>
   );
 };
