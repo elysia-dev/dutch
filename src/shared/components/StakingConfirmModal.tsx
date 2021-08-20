4812cc4504723b635975e56283e5014079754c4c
정규철 <stussy@jeonggyheolsMBP.kornet>
2021-08-20 15:05:23 +0900

    Add staking confirm modal

---
+++ src/shared/components/StakingConfirmModal.tsx
@@ -0,0 +1,219 @@
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import {
+  Modal,
+  View,
+  TouchableOpacity,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import { useTranslation } from 'react-i18next';
+import AppFonts from '../../enums/AppFonts';
+import AppColors from '../../enums/AppColors';
+import { H3Text, H4Text } from './Texts';
+
+interface Props {
+  modalVisible: boolean;
+  setModalVisible: Dispatch<SetStateAction<boolean>>;
+  title: string;
+  subtitle: string;
+  list: { label: string; value: string; subvalue?: string }[];
+  isApproved: boolean;
+  submitButtonText: string;
+  handler: () => void;
+  isLoading: boolean;
+}
+
+const StakingConfrimModal: React.FC<Props> = ({
+  modalVisible,
+  setModalVisible,
+  title,
+  subtitle,
+  list,
+  isApproved,
+  submitButtonText,
+  handler,
+  isLoading,
+}) => {
+  const { t } = useTranslation();
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <Modal transparent={true} visible={modalVisible} animationType={'slide'}>
+      <View
+        style={{
+          display: 'flex',
+          width: '100%',
+          height: '100%',
+        }}>
+        <TouchableOpacity
+          onPress={() => setModalVisible(false)}
+          activeOpacity={1}
+          style={{
+            flex: 1,
+            backgroundColor: AppColors.MODAL_BACKGROUND,
+          }}
+        />
+        <View
+          style={{
+            width: '100%',
+            height: 620,
+            display: 'flex',
+            backgroundColor: AppColors.MODAL_BACKGROUND,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              backgroundColor: AppColors.BACKGROUND_GREY,
+              padding: 20,
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+            }}>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <H4Text
+                label={t('assets.cancel')}
+                style={{ color: AppColors.MAIN }}
+              />
+            </TouchableOpacity>
+            <H3Text label={title} style={{}} />
+            <View style={{ width: 20 }} />
+          </View>
+          <View
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              padding: 18,
+              flex: 1,
+              backgroundColor: AppColors.WHITE,
+            }}>
+            <View>
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: AppColors.BLACK,
+                  textAlign: 'center',
+                  lineHeight: 32,
+                  fontWeight: 'bold',
+                  borderBottomWidth: 1,
+                  borderBottomColor: AppColors.GREY,
+                  alignItems: 'center',
+                  marginTop: 20,
+                  height: 50,
+                  fontFamily: AppFonts.Bold,
+                }}>
+                {subtitle}
+              </Text>
+              {list.map((item) => {
+                return (
+                  <View
+                    key={item.label}
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      borderBottomWidth: 1,
+                      borderBottomColor: AppColors.GREY,
+                      alignItems: 'center',
+                      height: item.subvalue ? 64 : 50,
+                      paddingHorizontal: 5,
+                    }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: AppColors.BLACK2,
+                        fontFamily: AppFonts.Regular,
+                      }}>
+                      {item.label}
+                    </Text>
+                    <View>
+                      <Text
+                        style={{
+                          fontSize: 14,
+                          color: AppColors.BLACK,
+                          textAlign: 'right',
+                          fontFamily: AppFonts.Bold,
+                        }}>
+                        {item.value}
+                      </Text>
+                      {item.subvalue && (
+                        <Text
+                          style={{
+                            fontSize: 12,
+                            color: AppColors.SUB_BLACK,
+                            textAlign: 'right',
+                            fontFamily: AppFonts.Regular,
+                          }}>
+                          {item.subvalue}
+                        </Text>
+                      )}
+                    </View>
+                  </View>
+                );
+              })}
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: AppColors.BLACK,
+                  marginHorizontal: 5,
+                  marginTop: 12,
+                  lineHeight: 20,
+                  fontFamily: AppFonts.Regular,
+                }}>
+                {`* ${t('assets.confirm_gas')}`}
+              </Text>
+            </View>
+            <View>
+              {!isApproved && (
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: AppColors.BLACK,
+                    marginHorizontal: 5,
+                    marginBottom: 12,
+                    lineHeight: 20,
+                    fontFamily: AppFonts.Bold,
+                  }}>
+                  {`* ${t('assets.check_allowance_guide')}`}
+                </Text>
+              )}
+              <TouchableOpacity
+                onPress={() => {
+                  setDisabled(true);
+                  handler();
+                }}
+                disabled={isLoading}
+                style={{
+                  backgroundColor: isLoading ? AppColors.GREY : AppColors.MAIN,
+                  borderRadius: 5,
+                  justifyContent: 'center',
+                  alignContent: 'center',
+                  height: 50,
+                }}>
+                {isLoading ? (
+                  <ActivityIndicator size="small" />
+                ) : (
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      textAlign: 'center',
+                      fontFamily: AppFonts.Bold,
+                      color: AppColors.WHITE,
+                    }}
+                    allowFontScaling={false}>
+                    {isApproved
+                      ? submitButtonText
+                      : t('assets.check_allowance')}
+                  </Text>
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default StakingConfrimModal;
