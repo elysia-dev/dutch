96a06ee0c0f9ef7a20a725050d9311d87eb0c9cb
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-22 17:55:37 +0900

    Move stakingLising to Main dashboard

--- src/modules/dashboard/components/StakingListing.tsx
+++ src/modules/dashboard/components/StakingListing.tsx
@@ -27,75 +27,11 @@ import {
 import { StakingPool } from '@elysia-dev/contract-typechain';
 import { utils } from '@elysia-dev/contract-typechain/node_modules/ethers';
 
-const StakingListing: React.FC<{ user: any; isWalletUser: boolean }> = ({
-  user,
-  isWalletUser,
-}) => {
-  const { wallet } = useContext(WalletContext);
-  const userAddress = isWalletUser
-    ? wallet?.getFirstAddress()
-    : user.ethAddresses[0];
-  const [elStakingInfoBoxes, setElStakingInfoBoxes] = useState(
-    [] as React.ReactNode[],
-  );
-  const [elfiStakingInfoBoxes, setElfiStakingInfoBoxes] = useState(
-    [] as React.ReactNode[],
-  );
+const StakingListing: React.FC<{
+  elStakingInfoBoxes: any;
+  elfiStakingInfoBoxes: any;
+}> = ({ elStakingInfoBoxes, elfiStakingInfoBoxes }) => {
   const { t } = useTranslation();
-
-  async function getRoundData(type: CryptoType): Promise<void> {
-    let contract: StakingPool;
-    let infoBoxes: React.ReactNode[];
-    let setInfoBoxes: Dispatch<SetStateAction<React.ReactNode[]>>;
-    if (type === CryptoType.EL) {
-      contract = getStakingPoolContract(
-        EL_STAKING_POOL_ADDRESS,
-        wallet?.getFirstSigner(),
-      );
-      infoBoxes = elStakingInfoBoxes;
-      setInfoBoxes = setElStakingInfoBoxes;
-    } else {
-      contract = getStakingPoolContract(
-        ELFI_STAKING_POOL_ADDRESS,
-        wallet?.getFirstSigner(),
-      );
-      infoBoxes = elfiStakingInfoBoxes;
-      setInfoBoxes = setElfiStakingInfoBoxes;
-    }
-
-    const tempBoxes = [] as React.ReactNode[];
-    for (let round = 1; round <= 6; round++) {
-      tempBoxes.push(
-        contract
-          .getUserData(round, userAddress)
-          .then((res: any) => {
-            const stakingAmount = Number(utils.formatEther(res[2])); // principal
-            const rewardAmount = Number(utils.formatEther(res[1]));
-            if (stakingAmount) {
-              return (
-                <StakingInfoBox
-                  key={round}
-                  cryptoType={type}
-                  round={round}
-                  stakingAmount={stakingAmount}
-                  rewardAmount={rewardAmount}
-                />
-              );
-            }
-          })
-          .catch((e) => {
-            console.log(e);
-          }),
-      );
-    }
-
-    setInfoBoxes(await Promise.all(tempBoxes));
-  }
-
-  useEffect(() => {
-    getRoundData(CryptoType.EL);
-    // getRoundData(CryptoType.ELFI);
-  }, []);
 
   return (
     <View
