13ce08e95cf8f0f1d963feae6676ebda4c0e5cf7
leegakyeong <alaska7132@naver.com>
2021-08-25 22:46:55 +0900

    Add full amount button to NumberPadShortcut

--- src/modules/staking/components/NumberPadShortcut.tsx
+++ src/modules/staking/components/NumberPadShortcut.tsx
@@ -1,21 +1,25 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
 import AppFonts from '../../../enums/AppFonts';
 import commaFormatter from '../../../utiles/commaFormatter';
 import AppColors from '../../../enums/AppColors';
 import decimalFormatter from '../../../utiles/decimalFormatter';
 
 interface Props {
-  values: number[];
+  values: (number | 'all')[];
   inputValue: string;
   setValue: Dispatch<SetStateAction<string>>;
+  maxValue?: number;
 }
 
 const NumberPadShortcut: React.FC<Props> = ({
   values,
   inputValue,
   setValue,
+  maxValue,
 }) => {
+  const { t } = useTranslation();
   const buttons = values.map((value) => {
     return (
       <TouchableOpacity
@@ -37,15 +41,21 @@ const NumberPadShortcut: React.FC<Props> = ({
             fontSize: 12,
             fontFamily: AppFonts.Medium,
           }}>
-          {`+${commaFormatter(value)}`}
+          {value === 'all'
+            ? t('staking.full_amount')
+            : `+${commaFormatter(value)}`}
         </Text>
       </TouchableOpacity>
     );
   });
 
-  function addValue(value: number) {
-    const newValue = parseFloat(inputValue || '0') + value;
-    setValue(decimalFormatter(newValue, 6));
+  function addValue(value: number | 'all') {
+    if (value === 'all') {
+      setValue(String(maxValue));
+    } else {
+      const newValue = parseFloat(inputValue || '0') + value;
+      setValue(decimalFormatter(newValue, 6));
+    }
   }
 
   return (
