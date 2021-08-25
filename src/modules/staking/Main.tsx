b7ae11a0c6e7f72b85aa0ae70d2edd0a94732538
leegakyeong <alaska7132@naver.com>
2021-08-25 17:38:35 +0900

    Add bounciness to staking/Main page (iOS only)

--- src/modules/staking/Main.tsx
+++ src/modules/staking/Main.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   Platform,
   Image,
+  ScrollView,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useTranslation } from 'react-i18next';
@@ -32,123 +33,125 @@ export const Main: React.FC = () => {
         backgroundColor: AppColors.WHITE,
       }}>
       <AnimatedMainHeader title={t('staking.staking')} scrollY={scrollY} />
-      <View
-        style={{
-          marginTop: 45,
-          alignItems: 'center',
-          paddingHorizontal: 20,
-        }}>
-        <TouchableCardWithShadow
+      <ScrollView alwaysBounceVertical={true}>
+        <View
           style={{
-            width: '100%',
-            height: 180,
-            marginBottom: 30,
-          }}
-          onPress={() => {
-            navigation.navigate(Page.Staking, {
-              screen: StakingPage.CurrentDashboard,
-              params: {
-                cryptoType: CryptoType.EL,
-                rewardCryptoType: CryptoType.ELFI,
-              },
-            });
+            marginTop: 45,
+            alignItems: 'center',
+            paddingHorizontal: 20,
           }}>
-          <View
+          <TouchableCardWithShadow
             style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
+              width: '100%',
+              height: 180,
+              marginBottom: 30,
+            }}
+            onPress={() => {
+              navigation.navigate(Page.Staking, {
+                screen: StakingPage.CurrentDashboard,
+                params: {
+                  cryptoType: CryptoType.EL,
+                  rewardCryptoType: CryptoType.ELFI,
+                },
+              });
             }}>
-            <Image
-              source={require('./images/el_staking_visualization.png')}
-              width={304}
-            />
-          </View>
-          <View
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('./images/el_staking_visualization.png')}
+                width={304}
+              />
+            </View>
+            <View
+              style={{
+                height: 65,
+                borderTopWidth: 1,
+                borderTopColor: AppColors.GREY,
+                padding: 16,
+              }}>
+              <Text
+                style={{
+                  color: AppColors.BLACK,
+                  fontSize: 15,
+                  lineHeight: 17,
+                  fontFamily: AppFonts.Bold,
+                }}>
+                {t('staking.staking_with_type', {
+                  stakingCrypto: CryptoType.EL,
+                })}
+              </Text>
+              <Text
+                style={{
+                  color: AppColors.BLACK,
+                  fontSize: 10,
+                  lineHeight: Platform.OS === 'ios' ? 24 : 20,
+                  fontFamily: AppFonts.Regular,
+                }}>
+                {`${t('staking.schedule')} : ${totalStakingPeriod}`}
+              </Text>
+            </View>
+          </TouchableCardWithShadow>
+          <TouchableCardWithShadow
             style={{
-              height: 65,
-              borderTopWidth: 1,
-              borderTopColor: AppColors.GREY,
-              padding: 16,
+              width: '100%',
+              height: 180,
+              marginBottom: 30,
+            }}
+            onPress={() => {
+              navigation.navigate(Page.Staking, {
+                screen: StakingPage.CurrentDashboard,
+                params: {
+                  cryptoType: CryptoType.ELFI,
+                  rewardCryptoType: CryptoType.DAI,
+                },
+              });
             }}>
-            <Text
+            <View
               style={{
-                color: AppColors.BLACK,
-                fontSize: 15,
-                lineHeight: 17,
-                fontFamily: AppFonts.Bold,
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
               }}>
-              {t('staking.staking_with_type', {
-                stakingCrypto: CryptoType.EL,
-              })}
-            </Text>
-            <Text
+              <Image
+                source={require('./images/elfi_staking_visualization.png')}
+                width={304}
+              />
+            </View>
+            <View
               style={{
-                color: AppColors.BLACK,
-                fontSize: 10,
-                lineHeight: Platform.OS === 'ios' ? 24 : 20,
-                fontFamily: AppFonts.Regular,
+                height: 65,
+                borderTopWidth: 1,
+                borderTopColor: AppColors.GREY,
+                padding: 16,
               }}>
-              {`${t('staking.schedule')} : ${totalStakingPeriod}`}
-            </Text>
-          </View>
-        </TouchableCardWithShadow>
-        <TouchableCardWithShadow
-          style={{
-            width: '100%',
-            height: 180,
-            marginBottom: 30,
-          }}
-          onPress={() => {
-            navigation.navigate(Page.Staking, {
-              screen: StakingPage.CurrentDashboard,
-              params: {
-                cryptoType: CryptoType.ELFI,
-                rewardCryptoType: CryptoType.DAI,
-              },
-            });
-          }}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Image
-              source={require('./images/elfi_staking_visualization.png')}
-              width={304}
-            />
-          </View>
-          <View
-            style={{
-              height: 65,
-              borderTopWidth: 1,
-              borderTopColor: AppColors.GREY,
-              padding: 16,
-            }}>
-            <Text
-              style={{
-                color: AppColors.BLACK,
-                fontSize: 15,
-                lineHeight: 17,
-                fontFamily: AppFonts.Bold,
-              }}>
-              {t('staking.staking_with_type', {
-                stakingCrypto: CryptoType.ELFI,
-              })}
-            </Text>
-            <Text
-              style={{
-                color: AppColors.BLACK,
-                fontSize: 10,
-                lineHeight: Platform.OS === 'ios' ? 24 : 20,
-                fontFamily: AppFonts.Regular,
-              }}>
-              {`${t('staking.schedule')} : ${totalStakingPeriod}`}
-            </Text>
-          </View>
-        </TouchableCardWithShadow>
-      </View>
+              <Text
+                style={{
+                  color: AppColors.BLACK,
+                  fontSize: 15,
+                  lineHeight: 17,
+                  fontFamily: AppFonts.Bold,
+                }}>
+                {t('staking.staking_with_type', {
+                  stakingCrypto: CryptoType.ELFI,
+                })}
+              </Text>
+              <Text
+                style={{
+                  color: AppColors.BLACK,
+                  fontSize: 10,
+                  lineHeight: Platform.OS === 'ios' ? 24 : 20,
+                  fontFamily: AppFonts.Regular,
+                }}>
+                {`${t('staking.schedule')} : ${totalStakingPeriod}`}
+              </Text>
+            </View>
+          </TouchableCardWithShadow>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
