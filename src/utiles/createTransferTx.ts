a55d63f3b3d52c7898b3d880fd2edb403562f24e
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-26 12:46:38 +0900

    Modify accelerate comment & erccontract hooks

--- src/utiles/createTransferTx.ts
+++ src/utiles/createTransferTx.ts
@@ -1,69 +1,133 @@
-import { ethers } from 'ethers';
-import Wallet from '../core/Wallet';
-import Accelerate from '../enums/Accelerate';
+import React, { useContext, useEffect, useState } from 'react';
 import CryptoType from '../enums/CryptoType';
 import TransferType from '../enums/TransferType';
-import CryptoTransaction from '../types/CryptoTransaction';
 import { purchaseProduct, sendCryptoAsset } from './createTransction';
+import useTxHandler from '../hooks/useTxHandler';
+import PriceContext from '../contexts/PriceContext';
+import WalletContext from '../contexts/WalletContext';
+import { TransactionResponse } from '@ethersproject/providers';
+import NetworkType from '../enums/NetworkType';
+import { useNavigation } from '@react-navigation/native';
+import { Contract } from '@ethersproject/contracts';
+import useErcContract from '../hooks/useErcContract';
 
-export default async function createTransferTx(
-  gasPrice: string,
-  bscGasPrice: string,
-  getCryptoPrice: (cryptoType: CryptoType) => number,
-  wallet: Wallet | undefined,
-  addPendingTransaction: (
-    txRes: ethers.providers.TransactionResponse | undefined,
-    tx: CryptoTransaction,
-  ) => void,
+export default function createTransferTx(
   type: CryptoType,
   transferType: TransferType,
-  productId?: number,
-  contract?: ethers.Contract | null,
-  valuesFrom?: string,
-  valuesTo?: string,
-  accelerate?: Accelerate,
-  address?: string,
-  updateGasPrice?: string,
+  contract: Contract | null,
+  // gasPrice: string,
+  // bscGasPrice: string,
+  // getCryptoPrice: (cryptoType: CryptoType) => number,
+  // wallet: Wallet | undefined,
+  // addPendingTransaction: (
+  //   txRes: ethers.providers.TransactionResponse | undefined,
+  //   tx: CryptoTransaction,
+  // ) => void,
+  // productId?: number,
+  // valuesFrom?: string,
+  // valuesTo?: string,
+  // accelerate?: Accelerate,
+  // address?: string,
+  // updateGasPrice?: string,
 ) {
-  try {
-    let txRes: ethers.providers.TransactionResponse | undefined;
-    if (transferType === TransferType.Purchase) {
-      txRes = await purchaseProduct(
-        gasPrice,
-        bscGasPrice,
-        type,
-        contract || null,
-        valuesFrom || '',
-        wallet,
-        getCryptoPrice,
-        updateGasPrice,
-        accelerate,
-      );
+  const { gasPrice, bscGasPrice, getCryptoPrice } = useContext(PriceContext);
+  const { wallet } = useContext(WalletContext);
+  // const { addPendingTransaction } = useContext(TransactionContext);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [resTx, setResTx] = useState<TransactionResponse>();
+  const { afterTxHashCreated, afterTxCreated } = useTxHandler();
+  const navigation = useNavigation();
+  const { elContract, elfiContract, daiContract } = useErcContract();
+
+  const setErcContract = (type: CryptoType) => {
+    if (type === CryptoType.ELFI) {
+      return elfiContract;
+    } else if (type === CryptoType.DAI) {
+      return daiContract;
     } else {
-      txRes = await sendCryptoAsset(
-        gasPrice,
-        bscGasPrice,
-        type,
-        address,
-        valuesTo,
-        wallet,
-        updateGasPrice,
-        accelerate,
-      );
+      return elContract;
     }
-    const isTransferTypePurchase = transferType === TransferType.Purchase;
-    addPendingTransaction(txRes, {
-      txHash: txRes?.hash,
-      cryptoType: isTransferTypePurchase ? CryptoType.ELA : type,
-      value: valuesTo || '',
-      createdAt: '',
-      type: isTransferTypePurchase ? 'in' : 'out',
-      blockNumber: 0,
-      productId: isTransferTypePurchase ? productId : 0,
-      toAddress: address,
-      valueFrom: isTransferTypePurchase ? valuesFrom : '',
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  };
+  const transferValue = async (
+    valuesFrom: string,
+    valuesTo: string,
+    address?: string,
+  ) => {
+    try {
+      setIsLoading(true);
+      if (transferType === TransferType.Purchase) {
+        setResTx(
+          await purchaseProduct(
+            gasPrice,
+            bscGasPrice,
+            type,
+            contract || null,
+            valuesFrom || '',
+            wallet,
+            getCryptoPrice,
+            // updateGasPrice,
+            // accelerate,
+          ),
+        );
+      } else {
+        let ercContract = setErcContract(type);
+        setResTx(
+          await sendCryptoAsset(
+            gasPrice,
+            bscGasPrice,
+            type,
+            address,
+            valuesTo,
+            wallet,
+            ercContract,
+            // updateGasPrice,
+            // accelerate,
+          ),
+        );
+      }
+      // const isTransferTypePurchase = transferType === TransferType.Purchase;
+      // addPendingTransaction(txRes, {
+      //   txHash: txRes?.hash,
+      //   cryptoType: isTransferTypePurchase ? CryptoType.ELA : type,
+      //   value: valuesTo || '',
+      //   createdAt: '',
+      //   type: isTransferTypePurchase ? 'in' : 'out',
+      //   blockNumber: 0,
+      //   productId: isTransferTypePurchase ? productId : 0,
+      //   toAddress: address,
+      //   valueFrom: isTransferTypePurchase ? valuesFrom : '',
+      // });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const waitTx = async () => {
+    try {
+      await resTx?.wait();
+      afterTxCreated(resTx?.hash || '', NetworkType.ETH);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const noticeTxStatus = () => {
+    afterTxHashCreated(
+      resTx?.from || '',
+      resTx?.to || '',
+      resTx?.hash || '',
+      NetworkType.ETH,
+    );
+  };
+
+  useEffect(() => {
+    if (resTx) {
+      setIsLoading(false);
+      navigation.goBack();
+      noticeTxStatus();
+      waitTx();
+    }
+  }, [resTx]);
+
+  return { isLoading, transferValue };
 }
