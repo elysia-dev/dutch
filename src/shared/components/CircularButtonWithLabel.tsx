34f9b4898a2936270e95e60c4c324889fbed465d
leegakyeong <alaska7132@naver.com>
2021-08-26 12:08:49 +0900

    Adjust CircularButtonWithLabel design - Break line of long texts in zh-hans

--- src/shared/components/CircularButtonWithLabel.tsx
+++ src/shared/components/CircularButtonWithLabel.tsx
@@ -37,7 +37,7 @@ const CircularButtonWithLabel: React.FC<{
   }
 
   return (
-    <View>
+    <View style={{ alignItems: 'center', flex: 1 }}>
       <TouchableOpacity disabled={disabled} onPress={pressHandler}>
         <Image
           source={source}
