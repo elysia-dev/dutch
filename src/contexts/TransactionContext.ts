13c068968ba2dd771979d0dec64c5e6613fee146
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-08-27 18:35:30 +0900

    Add isSuccessTx state

--- src/contexts/TransactionContext.ts
+++ src/contexts/TransactionContext.ts
@@ -5,24 +5,28 @@ import CryptoTransaction from '../types/CryptoTransaction';
 export type TransactionType = {
   transactions: CryptoTransaction[];
   counter: number;
+  isSuccessTx: boolean;
 };
 
 export interface ITransactionContext extends TransactionType {
-  
   addPendingTransaction: (
     txRes: ethers.providers.TransactionResponse | undefined,
     tx: CryptoTransaction,
   ) => void;
+  setIsSuccessTx: (isSuccessTx: boolean) => void;
 }
 
 export const initialTransactions = {
   transactions: [],
+
   counter: 0,
+  isSuccessTx: false,
 };
 
 export const initialTransactionContext = {
   ...initialTransactions,
   addPendingTransaction: () => {},
+  setIsSuccessTx: () => {},
 };
 
 const TransactionContext = createContext<ITransactionContext>(
