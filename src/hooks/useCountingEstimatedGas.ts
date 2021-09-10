dc196f5579e19781cca2e10a92f312a51381e0e3
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-09-10 18:27:52 +0900

    Modify useCount to useCountingEstimatedGas

---
+++ src/hooks/useCountingEstimatedGas.ts
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+import react, { useEffect, useState } from 'react';
+import CryptoType from '../enums/CryptoType';
+import StakingType from '../enums/StakingType';
+import useBoolean from './useBoolean';
+
+const useCountingEstimatedGas = (
+  setEstimateGas: (stakingType?: StakingType, round?: number) => Promise<void>,
+  assetType?: CryptoType | '',
+  stakingType?: StakingType,
+) => {
+  const [count, setCount] = useState(0);
+  const [isApproved, setIsApproved] = useBoolean(assetType);
+  const [isLoading, setIsLoading] = useBoolean();
+
+  const approveTxSucOrTimeOut = () => {
+    setIsApproved(true);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (count === 0 || count >= 4) return;
+    setTimeout(async () => {
+      try {
+        await setEstimateGas(stakingType);
+        approveTxSucOrTimeOut();
+      } catch (error) {
+        setAddCount();
+      } finally {
+        if (count >= 3) {
+          approveTxSucOrTimeOut();
+        }
+      }
+    }, 2000);
+  }, [count]);
+
+  const setAddCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return { setAddCount, isApproved, setIsApproved, isLoading, setIsLoading };
+};
+
+export default useCountingEstimatedGas;
