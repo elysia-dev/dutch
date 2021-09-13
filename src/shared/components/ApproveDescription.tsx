aa502191be8e49f5c60b52f3389986c937b7b93a
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-09-13 09:33:38 +0900

    Remove useEffect

--- src/shared/components/ApproveDescription.tsx
+++ src/shared/components/ApproveDescription.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
 import AppColors from '../../enums/AppColors';
