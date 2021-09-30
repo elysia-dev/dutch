81920e2ddd4a1b32c0577edb3140ebf5a660ff18
leegakyeong <alaska7132@naver.com>
2021-09-30 16:28:31 +0900

    Implement useUserAddress()

---
+++ src/hooks/useUserAddress.ts
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import UserContext from '../contexts/UserContext';
+import WalletContext from '../contexts/WalletContext';
+
+const useUserAddress = () => {
+  const { user, isWalletUser } = useContext(UserContext);
+  const { wallet } = useContext(WalletContext);
+
+  const userAddress = isWalletUser
+    ? wallet?.getFirstAddress()
+    : user.ethAddresses[0];
+
+  return userAddress;
+};
+
+export default useUserAddress;
