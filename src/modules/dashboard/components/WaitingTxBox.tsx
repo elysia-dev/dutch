569496f9c283950b43b8a6e69d7eef9b5f896242
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-01 18:39:16 +0900

    Add main dashboard to waitingTxBox component

--- src/modules/dashboard/components/WaitingTxBox.tsx
+++ src/modules/dashboard/components/WaitingTxBox.tsx
@@ -1,45 +1,77 @@
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { AppState, Text, TouchableOpacity, View } from 'react-native';
 import AppColors from '../../../enums/AppColors';
 import { DashboardPage, Page } from '../../../enums/pageEnum';
 import useWaitTx from '../../../hooks/useWaitTx';
 import { H4Text, P3Text } from '../../../shared/components/Texts';
 
-const WaitingTxBox = () => {
+const WaitingTxBox: React.FC<{
+  isFocused: boolean;
+}> = ({ isFocused }) => {
   const navigation = useNavigation();
-  const { waitingTxs } = useWaitTx();
+  const { waitingTxs, getStorageTx, removeStorageTxByAppState } = useWaitTx();
+
+  const changeAppState = async () => {
+    if (!waitingTxs) return;
+    await removeStorageTxByAppState();
+    await getStorageTx();
+  };
+
+  useEffect(() => {
+    AppState.addEventListener('change', changeAppState);
+
+    return () => {
+      AppState.removeEventListener('change', changeAppState);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isFocused) {
+      changeAppState();
+    }
+  }, [isFocused]);
 
   return (
-    <View
-      style={{
-        width: '100%',
-        height: 82,
-        backgroundColor: AppColors.GREY4,
-        justifyContent: 'center',
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingTop: 23,
-        paddingBottom: 22,
-        borderRadius: 5,
-        marginBottom: 40,
-      }}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate(Page.Dashboard, {
-            screen: DashboardPage.WaitingTxList,
-          });
-        }}>
-        <P3Text
-          label={'대기 중인 트랜잭션'}
-          style={{ color: AppColors.SUB_BLACK }}
+    <>
+      {waitingTxs.length === 0 ? (
+        <View
+          style={{
+            marginBottom: 40,
+          }}
         />
-        <H4Text
-          label={`현재 ${waitingTxs?.length || 0}건이 대기 중입니다.`}
-          style={{ fontSize: 15 }}
-        />
-      </TouchableOpacity>
-    </View>
+      ) : (
+        <View
+          style={{
+            width: '100%',
+            height: 82,
+            backgroundColor: AppColors.GREY4,
+            justifyContent: 'center',
+            paddingLeft: 20,
+            paddingRight: 20,
+            paddingTop: 23,
+            paddingBottom: 22,
+            borderRadius: 5,
+            marginBottom: 40,
+          }}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate(Page.Dashboard, {
+                screen: DashboardPage.WaitingTxList,
+              });
+            }}>
+            <P3Text
+              label={'대기 중인 트랜잭션'}
+              style={{ color: AppColors.SUB_BLACK }}
+            />
+            <H4Text
+              label={`현재 ${waitingTxs?.length || 0}건이 대기 중입니다.`}
+              style={{ fontSize: 15 }}
+            />
+          </TouchableOpacity>
+        </View>
+      )}
+    </>
   );
 };
 
