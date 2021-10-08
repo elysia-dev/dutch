603055a371b4ca74adfae8f2ae158edfcd2fe227
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-08 21:38:18 +0900

    Modify transaction context

--- src/contexts/TransactionContext.ts
+++ src/contexts/TransactionContext.ts
@@ -1,32 +1,45 @@
+import { TransactionResponse } from '@ethersproject/providers';
 import { ethers } from 'ethers';
 import { createContext } from 'react';
+import CryptoType from '../enums/CryptoType';
+import StakingType from '../enums/StakingType';
+import ToastStatus from '../enums/ToastStatus';
+import TransferType from '../enums/TransferType';
 import CryptoTransaction from '../types/CryptoTransaction';
+import { WaitingTransaction } from '../types/WaitingTransaction';
 
 export type TransactionType = {
   transactions: CryptoTransaction[];
+  waitingTxs: WaitingTransaction[];
   counter: number;
-  isSuccessTx: boolean;
 };
 
 export interface ITransactionContext extends TransactionType {
-  addPendingTransaction: (
-    txRes: ethers.providers.TransactionResponse | undefined,
-    tx: CryptoTransaction,
+  setWaitingTx: (
+    transferType: TransferType | StakingType,
+    amount: string,
+    resTx?: TransactionResponse,
+    cryptoType?: CryptoType,
+    productUnit?: string,
+    isSuccess?: boolean,
   ) => void;
-  setIsSuccessTx: (isSuccessTx: boolean) => void;
+  removeStorageTx: (txHash?: string) => void;
+  removeStorageTxByAppState: () => void;
+  setToastList: (type: string, status: ToastStatus) => void;
 }
 
 export const initialTransactions = {
   transactions: [],
-
+  waitingTxs: [],
   counter: 0,
-  isSuccessTx: false,
 };
 
 export const initialTransactionContext = {
   ...initialTransactions,
-  addPendingTransaction: () => {},
-  setIsSuccessTx: () => {},
+  setWaitingTx: () => {},
+  removeStorageTx: () => {},
+  removeStorageTxByAppState: () => {},
+  setToastList: () => {},
 };
 
 const TransactionContext = createContext<ITransactionContext>(
