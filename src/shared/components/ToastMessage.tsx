72d2f92a94fe62793cc7d64b169186855a03ba90
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-08 19:37:38 +0900

    Add ToastMessage component

--- src/shared/components/ToastMessage.tsx
+++ src/shared/components/ToastMessage.tsx
@@ -1,164 +1,144 @@
-import React, { useRef, useEffect, useContext } from 'react';
+import React, { useRef, useState, useEffect, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Animated, View, Image, TouchableOpacity } from 'react-native';
 import toastFailIcon from '../assets/images/toast_message_fail.png';
 import toastSuccessIcon from '../assets/images/toast_message_success.png';
 import closeIcon from '../assets/images/XCircleCloseDelete.png';
 import { P3Text } from './Texts';
 import AppColors from '../../enums/AppColors';
-import TransactionContext from '../../contexts/TransactionContext';
-import { WaitingTransaction } from '../../types/WaitingTransaction';
+import { ToastTransaction } from '../../types/WaitingTransaction';
+import TransferType from '../../enums/TransferType';
 
 type Props = {
-  waitingTx: WaitingTransaction;
-  idx: number;
+  waitingTx?: ToastTransaction;
+  toastHide: (id: number) => void;
 };
 
-const ToastMessage: React.FC<Props> = ({ waitingTx, idx }) => {
-  const txHash = waitingTx?.txHash;
+const ToastMessage: React.FC<Props> = ({ waitingTx, toastHide }) => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
-  const {
-    isSuccessTx,
-    isWaitingTx,
-    isFailedTx,
-    removePendingTransaction,
-    removeStorageTx,
-  } = useContext(TransactionContext);
+  const { t } = useTranslation();
+  const [isVisible, setIsVisible] = useState(true);
 
   const fadeIn = () => {
     Animated.timing(fadeAnim, {
       useNativeDriver: true,
       toValue: 1,
-      duration: 2000,
-    }).start();
+      duration: 1000,
+    }).start(() => {
+      const fadeOutTime = setTimeout(() => {
+        fadeOut();
+      }, 2000);
+      return () => clearTimeout(fadeOutTime);
+    });
   };
 
   const fadeOut = () => {
     Animated.timing(fadeAnim, {
       useNativeDriver: true,
       toValue: 0,
-      duration: 500,
+      duration: 1000,
     }).start(() => {
-      removePendingTransaction(txHash!);
+      setIsVisible(false);
     });
   };
 
   useEffect(() => {
     fadeIn();
-    setTimeout(() => {
-      fadeOut();
-    }, 3000);
+  }, [waitingTx]);
 
-    return () => clearTimeout();
-  }, []);
-
-  // useEffect(() => {
-  //   if (!isSuccessTx) return;
-  //   fadeIn();
-  //   setTimeout(() => {
-  //     fadeOut();
-  //   }, 3000);
-
-  //   return () => clearTimeout();
-  // }, [isSuccessTx]);
-
-  return (
-    <View
+  return isVisible ? (
+    <Animated.View
       style={{
-        position: 'absolute',
-        bottom: 100,
-        width: '100%',
+        // Bind opacity to animated value
+        width: '80%',
+        opacity: fadeAnim,
+        backgroundColor: AppColors.BLACK3,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        marginBottom: 60 * idx,
+        paddingHorizontal: 11,
+        paddingVertical: 15,
+        borderRadius: 10,
+        marginBottom: 15,
       }}>
-      <Animated.View
-        style={{
-          // Bind opacity to animated value
-          width: '80%',
-          opacity: fadeAnim,
-          backgroundColor: AppColors.BLACK3,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          paddingHorizontal: 11,
-          paddingVertical: 15,
-          borderRadius: 10,
-        }}>
-        {/* <Image
-          source={toastFailIcon}
-          style={{
-            width: 20,
-            height: 20,
-          }}
-        /> */}
-        {isSuccessTx ? (
-          <>
-            <Image
-              source={toastSuccessIcon}
-              style={{
-                width: 20,
-                height: 20,
-              }}
-            />
-            <P3Text
-              label={`부동산 토큰 구매가 완료하였습니다.`}
-              style={{
-                fontSize: 14,
-                fontFamily: 'NanumSquareEB',
-                color: AppColors.WHITE,
-              }}
-            />
-          </>
-        ) : isFailedTx ? (
-          <>
-            <Image
-              source={toastFailIcon}
-              style={{
-                width: 20,
-                height: 20,
-              }}
-            />
-            <P3Text
-              label={`트랜잭션을 전송했습니다.`}
-              style={{
-                fontSize: 14,
-                fontFamily: 'NanumSquareEB',
-                color: AppColors.WHITE,
-              }}
-            />
-          </>
-        ) : (
-          <>
-            <View
-              style={{
-                width: 20,
-                height: 20,
-              }}
-            />
-            <P3Text
-              label={`트랜잭션을 전송했습니다.`}
-              style={{
-                fontSize: 14,
-                fontFamily: 'NanumSquareEB',
-                color: AppColors.WHITE,
-              }}
-            />
-          </>
-        )}
-        <TouchableOpacity
-          onPress={() => {
-            removePendingTransaction(txHash!);
-          }}>
+      {waitingTx && waitingTx.isSuccessTx ? (
+        <>
           <Image
-            source={closeIcon}
+            source={toastSuccessIcon}
             style={{
-              width: 16,
-              height: 16,
+              width: 20,
+              height: 20,
             }}
           />
-        </TouchableOpacity>
-      </Animated.View>
-    </View>
-  );
+          <P3Text
+            label={
+              waitingTx.transferType === t(TransferType.Purchase)
+                ? t('transaction.purchase_transaction', {
+                    transferType: `${t(waitingTx.transferType)}`,
+                  })
+                : t('transaction.success_transaction', {
+                    transferType: `${t(waitingTx.transferType)}`,
+                  })
+            }
+            style={{
+              fontSize: 14,
+              fontFamily: 'NanumSquareEB',
+              color: AppColors.WHITE,
+            }}
+          />
+        </>
+      ) : waitingTx?.isFailTx ? (
+        <>
+          <Image
+            source={toastFailIcon}
+            style={{
+              width: 20,
+              height: 20,
+            }}
+          />
+          <P3Text
+            label={t('transaction.fail_transaction', {
+              transferType: `${t(waitingTx.transferType)}`,
+            })}
+            style={{
+              fontSize: 14,
+              fontFamily: 'NanumSquareEB',
+              color: AppColors.WHITE,
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <View
+            style={{
+              width: 20,
+              height: 20,
+            }}
+          />
+          <P3Text
+            label={t('transaction.create_transaction')}
+            style={{
+              fontSize: 14,
+              fontFamily: 'NanumSquareEB',
+              color: AppColors.WHITE,
+            }}
+          />
+        </>
+      )}
+      <TouchableOpacity
+        onPress={() => {
+          toastHide(waitingTx?.id!);
+        }}>
+        <Image
+          source={closeIcon}
+          style={{
+            width: 16,
+            height: 16,
+          }}
+        />
+      </TouchableOpacity>
+    </Animated.View>
+  ) : null;
 };
 
 export default ToastMessage;
