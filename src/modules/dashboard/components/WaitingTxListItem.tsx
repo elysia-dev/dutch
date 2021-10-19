09d8ddd15a9b48ce62431a2b257d4923c690f72c
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-19 14:57:48 +0900

    Modify WaitingTxListItem

--- src/modules/dashboard/components/WaitingTxListItem.tsx
+++ src/modules/dashboard/components/WaitingTxListItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import AppColors from '../../../enums/AppColors';
 import AppFonts from '../../../enums/AppFonts';
+import TransferType from '../../../enums/TransferType';
 import { P2Text, P3Text } from '../../../shared/components/Texts';
 import { WaitingTransaction } from '../../../types/WaitingTransaction';
 
@@ -13,52 +14,57 @@ const WaitingTxListItem: React.FC<WaitngTransactionItem> = ({
   waitingTransaction,
 }) => {
   const { transferType, date, amount, unit } = waitingTransaction;
-
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        borderBottomColor: AppColors.GREY,
-        borderBottomWidth: 1,
-        paddingBottom: 25,
-        marginBottom: 27,
-        paddingHorizontal: 9,
-        alignItems: 'center',
-      }}>
-      <View>
-        <P3Text
-          label={transferType}
-          style={{
-            fontFamily: AppFonts.Medium,
-            color: AppColors.BLACK,
-          }}
-        />
-        <P3Text
-          label={date}
-          style={{
-            fontSize: 11,
-            fontFamily: AppFonts.Medium,
-            color: AppColors.BLACK2,
-          }}
-        />
+    <>
+      {waitingTransaction.migrationInfo &&
+        waitingTransaction.migrationInfo.map((tx, idx) => {
+          return <WaitingTxListItem key={idx} waitingTransaction={tx} />;
+        })}
+      <View
+        style={{
+          flexDirection: 'row',
+          borderBottomColor: AppColors.GREY,
+          borderBottomWidth: 1,
+          paddingBottom: 25,
+          marginBottom: 27,
+          paddingHorizontal: 9,
+          alignItems: 'center',
+        }}>
+        <View>
+          <P3Text
+            label={transferType}
+            style={{
+              fontFamily: AppFonts.Medium,
+              color: AppColors.BLACK,
+            }}
+          />
+          <P3Text
+            label={date}
+            style={{
+              fontSize: 11,
+              fontFamily: AppFonts.Medium,
+              color: AppColors.BLACK2,
+            }}
+          />
+        </View>
+        <View style={{ marginLeft: 'auto', flexDirection: 'row' }}>
+          <P2Text
+            label={amount!}
+            style={{
+              fontFamily: AppFonts.Medium,
+              color: AppColors.BLACK,
+            }}
+          />
+          <P2Text
+            label={unit!}
+            style={{
+              fontFamily: AppFonts.Medium,
+              color: AppColors.TEXT_GREY2,
+            }}
+          />
+        </View>
       </View>
-      <View style={{ marginLeft: 'auto', flexDirection: 'row' }}>
-        <P2Text
-          label={amount!}
-          style={{
-            fontFamily: AppFonts.Medium,
-            color: AppColors.BLACK,
-          }}
-        />
-        <P2Text
-          label={unit!}
-          style={{
-            fontFamily: AppFonts.Medium,
-            color: AppColors.TEXT_GREY2,
-          }}
-        />
-      </View>
-    </View>
+    </>
   );
 };
 
