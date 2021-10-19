240c7d29af62d84ca771d723a6da9640a001e19f
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-19 15:08:38 +0900

    Add uuid data api

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-nested-ternary */
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import FlashMessage from 'react-native-flash-message';
+import { AppState } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { EXTERNAL_WALLET_TX_URL } from 'react-native-dotenv';
 import { More } from './src/modules/more/More';
 import Products from './src/modules/products';
 import { Account } from './src/modules/account/Account';
@@ -21,13 +24,59 @@ import Asset from './src/modules/asset';
 import Crypto from './src/modules/crypto';
 import Staking from './src/modules/staking';
 import TransactionContext from './src/contexts/TransactionContext';
-import { WaitingTransaction } from './src/types/WaitingTransaction';
+import { EXTERNAL_WALLET_UUID } from './src/constants/storage';
 
 const RootStack = createStackNavigator();
 
 const AppNavigator: React.FC = () => {
   const { signedIn, isWalletUser } = useContext(UserContext);
   const { isUnlocked } = useContext(WalletContext);
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+  const { verifyTx, waitingTxs, findSucceedTx } =
+    useContext(TransactionContext);
+
+  useEffect(() => {
+    AppState.addEventListener('change', (state) => {
+      appState.current = state;
+      setAppStateVisible(appState.current);
+    });
+
+    return () =>
+      AppState.removeEventListener('change', (state) => {
+        appState.current = state;
+        setAppStateVisible(appState.current);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (appStateVisible === 'active') {
+      if (waitingTxs.length) {
+        waitingTxs.map(async (tx) => {
+          findSucceedTx(tx);
+        });
+      }
+      if (isWalletUser) return;
+      (async () => {
+        const txUuid = await AsyncStorage.getItem(EXTERNAL_WALLET_UUID);
+        if (txUuid) {
+          const res = await axios.get(`${EXTERNAL_WALLET_TX_URL}/${txUuid}/tx`);
+          const txInfo = JSON.parse(String(res.data));
+          if (txInfo) {
+            verifyTx({
+              amount: txInfo.amount,
+              cryptoType: txInfo.cryptoType,
+              date: txInfo.date,
+              nonce: 0,
+              transferType: txInfo.transferType,
+              hash: txInfo.txHash,
+              unit: '',
+            });
+          }
+        }
+      })();
+    }
+  }, [appStateVisible]);
 
   return (
     <NavigationContainer>
